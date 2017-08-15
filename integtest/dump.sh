ac85aa57ce756f0c6779e90e21574aaa008614f8
Soup <maxiaoyu@pingcap.com>
2017-08-15 12:55:42 +0800

    Update dump.sh

--- integtest/dump.sh
+++ integtest/dump.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+BASEDIR=$(dirname "$0")
 CLASS="com.pingcap.spark.TestFramework"
 
 java -Dtest.mode=Dump -Dtest.dumpDB.databases="$@" -cp ${BASEDIR}/conf:${BASEDIR}/lib/* $CLASS 
