7db0ce60a0ed4c5b03f9a6747e54ca4944ce44a3
Novemser <novemser@gmail.com>
2017-11-10 13:23:57 +0800

    [Comment]Add more comment

--- python/pytispark.py
+++ python/pytispark.py
@@ -11,23 +11,35 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-#
 
 from py4j.java_gateway import java_import
 from pyspark.context import SparkContext
 
 
+# TiContext
+# Used for TiSpark
 class TiContext:
-    # create a new TiContext
+    """
+    Create a new TiContext
+    :param sparkSession The spark session used for creating TiContext
+    """
     def __init__(self, sparkSession):
         SparkContext._ensure_initialized()
         gw = SparkContext._gateway
         java_import(gw.jvm, "org.apache.spark.sql.TiContext")
         self.ti = gw.jvm.TiContext(sparkSession._jsparkSession)
 
+    """
+    Get the TiContext java representation
+    """
+
     def getContext(self):
         return self.ti
 
+    """
+    Change TiContext designated database
+    :param dbName Database to map(switch to)
+    :param isPrefix Whether to use dbName As Prefix
+    """
     def tidbMapDatabase(self, dbName, isPrefix):
         self.ti.tidbMapDatabase(dbName, isPrefix)
