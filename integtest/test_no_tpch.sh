97fe65cb506a2bb064798453ebbfc6ff54f14ced
birdstorm <samuelwyf@hotmail.com>
2017-12-01 03:29:59 +0800

    now supports running particular sql on particular db from script

--- integtest/test_no_tpch.sh
+++ integtest/test_no_tpch.sh
@@ -3,7 +3,7 @@ set -ue
 
 BASEDIR=$(cd `dirname $0`; pwd)
 echo "Base directory in: ${BASEDIR}"
-echo "Usage: <bin> [-a | -d | -h | -s | -i | -r]"
+echo "Usage: <bin> [-h | -g | -a | -d | -s | -i | -r | -t <sql> | -b <db>]"
 
 clear_last_diff_files() {
     for f in ./*.spark; do
@@ -21,8 +21,11 @@ clear_last_diff_files
 isDebug=false
 showResultStats=false
 showFailedOnly=false
+mode="Integration"
+sql=
+db=
 
-while getopts ":fradish" arg
+while getopts "t:b:dishrag" arg
 do
     case ${arg} in
         d)
@@ -31,7 +34,7 @@ do
 		r)
 			showResultStats=true
 			;;
-		f)
+		g)
 		    showFailedOnly=true
 		    ;;
         a)
@@ -51,15 +54,26 @@ do
         i)
             mvn clean install
             ;;
+        t)
+            sql=$OPTARG
+            echo "sql=$sql"
+            mode="QueryOnly"
+            ;;
+        b)
+            db=$OPTARG
+            echo "db=$db"
+            ;;
         h)
             echo "Options"
-            echo "  -a    make all projects"
-            echo "  -s    make tiSpark and integration test projects"
-            echo "  -i    make integration test only"
-            echo "  -r    show result stats (SQL, outputs, time consumed, etc.)"
-            echo "  -f    show failed only"
-            echo "  -d    debug mode"
-            echo "  -h    show help"
+            echo "  -a        make all projects"
+            echo "  -s        make tiSpark and integration test projects"
+            echo "  -i        make integration test only"
+            echo "  -r        show result stats (SQL, outputs, time consumed, etc.)"
+            echo "  -g        show failed only"
+            echo "  -t <sql>  run sql statement <sql> (with quotes) only on TiSpark with debug mode (must assign a database)"
+            echo "  -b <db>   use database <db> (with quotes) implicitly"
+            echo "  -d        debug mode"
+            echo "  -h        show help"
             exit 1
             ;;
         ?)
@@ -72,31 +86,54 @@ done
 
 CLASS="com.pingcap.spark.TestFramework"
 
-cp ${BASEDIR}/conf/tispark_config_testindex.properties.template ${SPARK_HOME}/conf/tispark_config.properties
+spark_cmd="${SPARK_HOME}/bin/spark-submit --class ${CLASS} ${BASEDIR}/lib/* --driver-java-options"
 spark_debug_opt="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=y"
 spark_test_opt=""
 
-spark_cmd="${SPARK_HOME}/bin/spark-submit --class ${CLASS} ${BASEDIR}/lib/* --driver-java-options"
+#exit -1
 
-filter=""
-
-if [ ${isDebug} = true ]; then
-    echo "debugging..."
-    ${spark_cmd} ${spark_debug_opt}
+if [ "${mode}" == "Integration" ]; then
+    filter=""
+    cp ${BASEDIR}/conf/tispark_config_testindex.properties.template ${SPARK_HOME}/conf/tispark_config.properties
+    if ![ -z "${db}" ]; then
+        echo "test.db=$db" >> ${SPARK_HOME}/conf/tispark_config.properties
+    fi
+    if [ ${isDebug} = true ]; then
+        echo "debugging..."
+        ${spark_cmd} ${spark_debug_opt}
+    else
+        echo "testing...."
+        if [ ${showResultStats} = true ]; then
+            if [ ${showFailedOnly} = true ]; then
+                filter="hint:\|output:\|Result:\|Elapsed time:\|query on spark\|query on TiDB\|FAILED."
+            else
+                filter="hint:\|output:\|Result:\|Elapsed time:\|query on spark\|query on TiDB\|FAILED.\|PASSED.\|SKIPPED.\|exception caught"
+            fi
+        else
+            if [ ${showFailedOnly} = true ]; then
+                filter="Tests result:\|Result:\|exception caught.\|FAILED."
+            else
+                filter="Tests result:\|Result:\|exception caught.\|FAILED.\|PASSED.\|SKIPPED."
+            fi
+        fi
+        ${spark_cmd} ${spark_test_opt} 2>&1 | grep "${filter}"
+    fi
+elif [ "${mode}" == "QueryOnly" ]; then
+    cp ${BASEDIR}/conf/tispark_config.properties.template ${SPARK_HOME}/conf/tispark_config.properties
+    if [ -z "${sql}" ]; then
+        echo "sql can not be empty. Aborting..."
+        exit -1
+    else
+        echo "test.sql=$sql" >> ${SPARK_HOME}/conf/tispark_config.properties
+    fi
+    if [ -z "${db}" ]; then
+        echo "DB name not specified. Aborting..."
+        exit -1
+    else
+        echo "test.db=$db" >> ${SPARK_HOME}/conf/tispark_config.properties
+    fi
+    echo "Running statement $sql"
+    ${spark_cmd} ${spark_test_opt} 2>&1 | grep "hint:\|output:\|Result:\|Elapsed time:\|query on\|exception caught"
 else
-    echo "testing...."
-	if [ ${showResultStats} = true ]; then
-	    if [ ${showFailedOnly} = true ]; then
-	        filter="hint:\|output:\|Result:\|Elapsed time:\|query on spark\|query on TiDB\|FAILED."
-	    else
-	        filter="hint:\|output:\|Result:\|Elapsed time:\|query on spark\|query on TiDB\|FAILED.\|PASSED.\|SKIPPED.\|exception caught"
-	    fi
-	else
-	    if [ ${showFailedOnly} = true ]; then
-	        filter="Tests result:\|Result:\|exception caught.\|FAILED."
-	    else
-	        filter="Tests result:\|Result:\|exception caught.\|FAILED.\|PASSED.\|SKIPPED."
-	    fi
-	fi
-	${spark_cmd} ${spark_test_opt} 2>&1 | grep "${filter}"
+    echo "UnKnown test mode: $mode. Aborting..."
 fi
