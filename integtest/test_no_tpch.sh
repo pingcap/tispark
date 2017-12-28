ddad00a8e8e6e21cdb2aeb7681711728115baca9
birdstorm <samuelwyf@hotmail.com>
2017-12-28 16:35:10 +0800

    fix test_no_tpch.sh

--- integtest/test_no_tpch.sh
+++ integtest/test_no_tpch.sh
@@ -32,7 +32,7 @@ do
 		    showFailedOnly=true
 		    ;;
         a)
-            cd ../tikv-client-lib-java/
+            cd ../tikv-client/
             mvn clean install
             cd ../
             mvn clean install
