437add75091c9836d3bf6cf9e584fe38b6c57e17
Ma Xiaoyu <maxiaoyu@pingcap.com>
2018-01-12 14:48:56 +0800

    revert comment

--- core/src/main/scala/com/pingcap/tispark/TiUtils.scala
+++ core/src/main/scala/com/pingcap/tispark/TiUtils.scala
@@ -198,11 +198,11 @@ object TiUtils {
       tiConf.setCommandPriority(priority)
     }
 
-    //if (conf.contains(TiConfigConst.REGION_INDEX_SCAN_DOWNGRADE_THRESHOLD)) {
-    //  tiConf.setRegionIndexScanDowngradeThreshold(
-    //    conf.get(TiConfigConst.REGION_INDEX_SCAN_DOWNGRADE_THRESHOLD).toLong
-    //  )
-    //}
+    if (conf.contains(TiConfigConst.REGION_INDEX_SCAN_DOWNGRADE_THRESHOLD)) {
+      tiConf.setRegionIndexScanDowngradeThreshold(
+        conf.get(TiConfigConst.REGION_INDEX_SCAN_DOWNGRADE_THRESHOLD).toLong
+      )
+    }
     tiConf
   }
 
