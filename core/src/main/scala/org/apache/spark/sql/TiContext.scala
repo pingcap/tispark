3e6dcf0c08c129e2864dd536a1581c3165a81d2a
Novemser <novemser@gmail.com>
2018-01-20 15:50:52 +0800

    Add region rebalance debug tool

--- core/src/main/scala/org/apache/spark/sql/TiContext.scala
+++ core/src/main/scala/org/apache/spark/sql/TiContext.scala
@@ -15,13 +15,19 @@
 
 package org.apache.spark.sql
 
+import java.{lang, util}
+
 import com.pingcap.tikv.tools.RegionUtils
 import com.pingcap.tikv.{TiConfiguration, TiSession}
 import com.pingcap.tispark._
 import org.apache.spark.SparkConf
 import org.apache.spark.internal.Logging
+import play.api.libs.json._
 
 import scala.collection.JavaConverters._
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+import scalaj.http.Http
 
 class TiContext(val session: SparkSession) extends Serializable with Logging {
   val sqlContext: SQLContext = session.sqlContext
@@ -39,6 +45,47 @@ class TiContext(val session: SparkSession) extends Serializable with Logging {
   class DebugTool {
     def getRegionDistribution(dbName: String, tableName: String): Map[String, Integer] = {
       RegionUtils.getRegionDistribution(tiSession, dbName, tableName).asScala.toMap
+    }
+
+    def balanceRegionByTable(pdAddr: String,
+                             dbName: String,
+                             tableName: String): Map[String, Integer] = {
+      val regionsPrefix = "pd/api/v1/regions"
+      val regionsWriteflowPrefix = "pd/api/v1/regions/writeflow"
+      val regionsReadflowPrefix = "pd/api/v1/regions/readflow"
+      val regionIDPrefix = "pd/api/v1/region/id"
+      val regionKeyPrefix = "pd/api/v1/region/key"
+      val storeRegionId = RegionUtils.getStoreRegionIdDistribution(tiSession, dbName, tableName)
+      val storeRegionCount = mutable.Map[Long, Long]()
+
+      storeRegionId.asScala.foreach((tuple: (lang.Long, java.util.List[lang.Long])) => {
+        storeRegionCount(tuple._1) = tuple._2.size()
+      })
+
+      storeRegionId.asScala
+        .flatMap(_._2)
+        .foreach((regionId: lang.Long) => {
+          val resStr = Http(s"$pdAddr/$regionIDPrefix/$regionId").asString
+          val json: JsValue = Json.parse(resStr.body)
+          val leader = json("leader").as[JsObject]
+          val peers = json("peers").as[JsArray].value
+          val leaderStoreId = leader("store_id").as[JsNumber].value.toLong
+
+          val targetLeaders = peers
+            .map { _("store_id").as[JsNumber].value.toLong }
+            .filterNot { _ == leaderStoreId }
+            .filter { id =>
+              storeRegionCount.contains(id) &&
+              storeRegionCount(id) + 10 < storeRegionCount(leaderStoreId)
+            }
+
+          if (targetLeaders.nonEmpty) {
+            println(
+              s"Transfer $regionId leader :Store $leaderStoreId to Store ${targetLeaders.head}"
+            )
+          }
+        })
+      getRegionDistribution(dbName, tableName)
     }
   }
 
