3f6b3211fe6582157f0981cf91a6ecb3971f4a80
birdstorm <samuelwyf@hotmail.com>
2018-02-12 15:05:48 +0800

    add more tests

--- core/src/test/scala/org/apache/spark/sql/expression/index/CoveringIndex0Suite.scala
+++ core/src/test/scala/org/apache/spark/sql/expression/index/CoveringIndex0Suite.scala
@@ -463,6 +463,8 @@ class CoveringIndex0Suite extends BaseTiSparkSuite {
     "select tp_int, tp_int from full_data_type_table_idx where tp_real > 0.7",
     "select id_dt, tp_real, id_dt from full_data_type_table_idx where tp_real between 0.5 and 0.7",
     "select tp_real, id_dt, tp_real from full_data_type_table_idx where tp_real > 0.7 and tp_int <= 2333",
+    // cover index tests: aggregations, group by, order by and limit
+    "select avg(tp_int) from full_data_type_table_idx where tp_int <= 2333 group by tp_tinyint order by tp_tinyint desc limit 20",
     // cover index tests: projection with expression
     "select tp_int + 500 from full_data_type_table_idx where tp_real > 0.7",
     "select -tp_int, tp_real from full_data_type_table_idx where id_dt > 40",
@@ -478,6 +480,7 @@ class CoveringIndex0Suite extends BaseTiSparkSuite {
     "select tp_int + 500 from full_data_type_table_idx where tp_real + 0.5 > 0.7",
     "select 1 + tp_int, tp_real / 3.14 from full_data_type_table_idx where id_dt * tp_real > 20",
     "select id_dt, tp_int - id_dt from full_data_type_table_idx where tp_real between tp_int and 0.7",
+    "select avg(tp_int/1000), last(id_dt) from full_data_type_table_idx where 2333 <= tp_int group by tp_tinyint order by tp_tinyint desc limit 20",
     // index double read:
     "select tp_int, tp_bigint from full_data_type_table_idx where tp_real > 0.7",
     "select tp_int, tp_real from full_data_type_table_idx where id_dt > 40 and tp_bigint < 0",
@@ -486,11 +489,11 @@ class CoveringIndex0Suite extends BaseTiSparkSuite {
   )
 
   allCases foreach { query =>
-  {
-    test(query) {
-      runTest(query, query.replace("full_data_type_table_idx", "full_data_type_table_idx_j"))
+    {
+      test(query) {
+        runTest(query, query.replace("full_data_type_table_idx", "full_data_type_table_idx_j"))
+      }
     }
-  }
   }
 
 }
