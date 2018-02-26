34ebbad96d4f43029211d55e7049f5aa8a80e6df
Novemser <novemser@gmail.com>
2018-02-26 18:38:44 +0800

    Add test cases

--- core/src/test/scala/org/apache/spark/sql/statistics/StatisticsManagerSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/statistics/StatisticsManagerSuite.scala
@@ -1,9 +1,9 @@
 package org.apache.spark.sql.statistics
 
 import com.google.common.collect.ImmutableList
-import com.pingcap.tikv.expression.ComparisonBinaryExpression.{equal, lessEqual}
+import com.pingcap.tikv.expression.ComparisonBinaryExpression._
 import com.pingcap.tikv.expression._
-import com.pingcap.tikv.meta.TiTableInfo
+import com.pingcap.tikv.meta.{TiIndexInfo, TiTableInfo}
 import com.pingcap.tikv.predicates.PredicateUtils.expressionToIndexRanges
 import com.pingcap.tikv.predicates.ScanAnalyzer
 import com.pingcap.tispark.statistics.StatisticsManager
@@ -43,47 +43,54 @@ class StatisticsManagerSuite extends BaseTiSparkSuite {
   test("select count(1) from full_data_type_table_idx where tp_int = 2006469139 or tp_int < 0") {
     val indexes = fDataIdxTbl.getIndices
     val idx = indexes.filter(_.getIndexColumns.asScala.exists(_.matchName("tp_int"))).head
+
     val eq1: Expression =
       equal(ColumnRef.create("tp_int", fDataIdxTbl), Constant.create(2006469139))
     val eq2: Expression = lessEqual(ColumnRef.create("tp_int", fDataIdxTbl), Constant.create(0))
     val or: Expression = LogicalBinaryExpression.or(eq1, eq2)
 
     val expressions = ImmutableList.of(or)
+    testSelectRowCount(expressions, idx, 46)
+  }
+
+  test("select tp_int from full_data_type_table_idx where tp_int < 5390653 and tp_int > -46759812") {
+    val indexes = fDataIdxTbl.getIndices
+    val idx = indexes.filter(_.getIndexColumns.asScala.exists(_.matchName("tp_int"))).head
+
+    val le1: Expression =
+      lessThan(ColumnRef.create("tp_int", fDataIdxTbl), Constant.create(5390653))
+    val gt: Expression = greaterThan(ColumnRef.create("tp_int", fDataIdxTbl), Constant.create(-46759812))
+    val and: Expression = LogicalBinaryExpression.and(le1, gt)
+
+    val expressions = ImmutableList.of(and)
+    testSelectRowCount(expressions, idx, 5)
+  }
+
+  def testSelectRowCount(expressions: Seq[Expression], idx: TiIndexInfo, expectedCount: Long):Unit = {
     val result = ScanAnalyzer.extractConditions(expressions, fDataIdxTbl, idx)
     val irs = expressionToIndexRanges(result.getPointPredicates, result.getRangePredicate)
     val tblStatistics = StatisticsManager.getInstance().getTableStatistics(fDataIdxTbl.getId)
     val idxStatistics = tblStatistics.getIndexHistMap.get(idx.getId)
     val rc = idxStatistics.getRowCount(irs).toLong
-    assert(rc == 46)
+    assert(rc == expectedCount)
   }
 
   // TODO: consider covering index cases
-  val cases = Map(
+  val indexSelectionCases = Map(
     "select id_dt from full_data_type_table_idx where tp_int = 2333" -> "idx_tp_int",
     "select tp_int from full_data_type_table_idx where tp_bigint < 10 and tp_int < 40" -> "idx_tp_int",
     "select tp_int from full_data_type_table_idx where tp_bigint < -4511898209778166952 and tp_int < 40" -> "idx_tp_bigint"
   )
 
-  cases.foreach((t: (String, String)) => {
+  indexSelectionCases.foreach((t: (String, String)) => {
     val query = t._1
     val idxName = t._2
     test(query) {
       val handleRDDExec = extractHandleRDDExec(query)
       val usedIdxName = extractUsedIndex(handleRDDExec)
-      assert(usedIdxName.eq(idxName))
+      assert(usedIdxName.equals(idxName))
     }
   })
-
-  test("Index scan test cases") {
-
-    //    val handleRDDExec = extractHandleRDDExec("select id_dt from full_data_type_table_idx where tp_int = 2333")
-    //    val idxName = extractUsedIndex(handleRDDExec)
-    //    println(idxName)
-
-    //    spark.sql("select tp_int from full_data_type_table_idx where tp_bigint < 10 and tp_int < 40").explain
-    //    spark.sql("select tp_int from full_data_type_table_idx where tp_bigint = 122222 and tp_int < 40").explain
-    //    spark.sql("select id_dt from full_data_type_table_idx where tp_int = 2333 and tp_bigint < 40").explain
-  }
 
   /**
     * Extract first handle rdd exec node from the given query
