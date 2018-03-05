c85aa4ce0a711a8239f715cd070886fccff1ad8d
Novemser <novemser@gmail.com>
2018-03-05 18:04:45 +0800

    Minor change

---
+++ core/src/main/scala/com/pingcap/tispark/statistics/StatisticsHelper.scala
@@ -0,0 +1,179 @@
+package com.pingcap.tispark.statistics
+
+import com.pingcap.tikv.expression.{ByItem, ColumnRef, ComparisonBinaryExpression, Constant}
+import com.pingcap.tikv.key.{Key, TypedKey}
+import com.pingcap.tikv.meta.TiDAGRequest.PushDownType
+import com.pingcap.tikv.meta.{TiDAGRequest, TiTableInfo}
+import com.pingcap.tikv.row.Row
+import com.pingcap.tikv.statistics.{Bucket, CMSketch, Histogram}
+import com.pingcap.tikv.types.{DataType, DataTypeFactory, MySQLType}
+import org.slf4j.LoggerFactory
+
+import scala.collection.JavaConversions._
+import scala.collection.mutable
+
+object StatisticsHelper {
+  private final lazy val logger = LoggerFactory.getLogger(getClass.getName)
+
+//  def loadStatisticInformation(tblId: Long)
+
+  def extractStatisticsDTO(row: Row,
+                           table: TiTableInfo,
+                           loadAll: Boolean,
+                           neededColIds: mutable.ArrayBuffer[Long]): StatisticsDTO = {
+    val isIndex = if (row.getLong(1) > 0) true else false
+    val histID = row.getLong(2)
+    val distinct = row.getLong(3)
+    val histVer = row.getLong(4)
+    val nullCount = row.getLong(5)
+    val cMSketch = row.getBytes(6)
+    val indexInfos = table.getIndices.filter(_.getId == histID)
+    val colInfos = table.getColumns.filter(_.getId == histID)
+    var needed = true
+
+    // we should only query those columns that user specified before
+    if (!loadAll && !neededColIds.contains(histID)) needed = false
+
+    var indexFlag = 1
+    var dataType: DataType = DataTypeFactory.of(MySQLType.TypeBlob)
+    // Columns info found
+    if (!isIndex && colInfos.nonEmpty) {
+      indexFlag = 0
+      dataType = colInfos.head.getType
+    } else if (!isIndex || indexInfos.isEmpty) {
+      logger.error(
+        s"We cannot find histogram id $histID in table info ${table.getName} now. It may be deleted."
+      )
+      needed = false
+    }
+
+    if (needed) {
+      StatisticsDTO(
+        histID,
+        indexFlag,
+        distinct,
+        histVer,
+        nullCount,
+        dataType,
+        cMSketch,
+        if (indexInfos.nonEmpty) indexInfos.head else null,
+        if (colInfos.nonEmpty) colInfos.head else null
+      )
+    } else {
+      null
+    }
+  }
+
+  def extractStatisticResult(histId: Long,
+                             rows: Iterator[Row],
+                             requests: Seq[StatisticsDTO]): StatisticsResult = {
+    val matches = requests.filter(_.colId == histId)
+    if (matches.nonEmpty) {
+      val matched = matches.head
+      var totalCount: Long = 0
+      val buckets = mutable.ArrayBuffer[Bucket]()
+      while (rows.hasNext) {
+        val row = rows.next()
+        val count = row.getLong(0)
+        val repeats = row.getLong(1)
+        var lowerBound: Key = null
+        var upperBound: Key = null
+        // all bounds are stored as blob in bucketTable currently, decode using blob type
+        lowerBound = TypedKey.toTypedKey(row.getBytes(2), DataTypeFactory.of(MySQLType.TypeBlob))
+        upperBound = TypedKey.toTypedKey(row.getBytes(3), DataTypeFactory.of(MySQLType.TypeBlob))
+        totalCount += count
+        buckets += new Bucket(totalCount, repeats, lowerBound, upperBound)
+      }
+      // create histogram for column `colId`
+      val histogram = Histogram
+        .newBuilder()
+        .setId(matched.colId)
+        .setNDV(matched.distinct)
+        .setNullCount(matched.nullCount)
+        .setLastUpdateVersion(matched.version)
+        .setBuckets(buckets)
+        .build()
+      // parse CMSketch
+      val rawData = matched.rawCMSketch
+      val cMSketch = if (rawData == null || rawData.length <= 0) {
+        null
+      } else {
+        val sketch = com.pingcap.tidb.tipb.CMSketch.parseFrom(rawData)
+        val result =
+          CMSketch.newCMSketch(sketch.getRowsCount, sketch.getRows(0).getCountersCount)
+        for (i <- 0 until sketch.getRowsCount) {
+          val row = sketch.getRows(i)
+          result.setCount(0)
+          for (j <- 0 until row.getCountersCount) {
+            val counter = row.getCounters(j)
+            result.getTable()(i)(j) = counter
+            result.setCount(result.getCount + counter)
+          }
+        }
+        result
+      }
+      StatisticsResult(histId, histogram, cMSketch, matched.idxInfo, matched.colInfo)
+    } else {
+      null
+    }
+  }
+
+  def buildHistogramsRequest(histTable: TiTableInfo,
+                             targetTblId: Long,
+                             startTs: Long): TiDAGRequest = {
+    TiDAGRequest.Builder
+      .newBuilder()
+      .setFullTableScan(histTable)
+      .addFilter(
+        ComparisonBinaryExpression
+          .equal(ColumnRef.create("table_id"), Constant.create(targetTblId))
+      )
+      .addRequiredCols(
+        "table_id",
+        "is_index",
+        "hist_id",
+        "distinct_count",
+        "version",
+        "null_count",
+        "cm_sketch"
+      )
+      .setStartTs(startTs)
+      .build(PushDownType.NORMAL)
+  }
+
+  def buildMetaRequest(metaTable: TiTableInfo, targetTblId: Long, startTs: Long): TiDAGRequest = {
+    TiDAGRequest.Builder
+      .newBuilder()
+      .setFullTableScan(metaTable)
+      .addFilter(
+        ComparisonBinaryExpression
+          .equal(ColumnRef.create("table_id"), Constant.create(targetTblId))
+      )
+      .addRequiredCols("table_id", "count", "modify_count", "version")
+      .setStartTs(startTs)
+      .build(PushDownType.NORMAL)
+  }
+
+  def buildBucketRequest(bucketTable: TiTableInfo, targetTblId: Long, startTs: Long): TiDAGRequest = {
+    TiDAGRequest.Builder
+      .newBuilder()
+      .setFullTableScan(bucketTable)
+      .addFilter(
+        ComparisonBinaryExpression
+          .equal(ColumnRef.create("table_id"), Constant.create(targetTblId))
+      )
+      .addOrderBy(ByItem.create(ColumnRef.create("bucket_id"), false))
+      .addRequiredCols(
+        "count",
+        "repeats",
+        "lower_bound",
+        "upper_bound",
+        "bucket_id",
+        "table_id",
+        "is_index",
+        "hist_id"
+      )
+      .setStartTs(startTs)
+      .build(PushDownType.NORMAL)
+  }
+}
--- core/src/main/scala/com/pingcap/tispark/statistics/StatisticsManager.scala
+++ core/src/main/scala/com/pingcap/tispark/statistics/StatisticsManager.scala
@@ -17,20 +17,15 @@
 
 package com.pingcap.tispark.statistics
 
-import java.lang
 import java.util.concurrent.TimeUnit
 
-import com.google.common.cache.{CacheBuilder, Weigher}
+import com.google.common.cache.{CacheBuilder, CacheLoader, Weigher}
+import com.google.common.util.concurrent.ListenableFuture
 import com.pingcap.tikv.TiSession
-import com.pingcap.tikv.expression.{ByItem, ColumnRef, ComparisonBinaryExpression, Constant}
-import com.pingcap.tikv.key.{Key, RowKey, TypedKey}
-import com.pingcap.tikv.kvproto.Coprocessor
-import com.pingcap.tikv.meta.TiDAGRequest.PushDownType
-import com.pingcap.tikv.meta.{TiColumnInfo, TiDAGRequest, TiIndexInfo, TiTableInfo}
+import com.pingcap.tikv.meta.{TiColumnInfo, TiIndexInfo, TiTableInfo}
 import com.pingcap.tikv.row.Row
 import com.pingcap.tikv.statistics._
-import com.pingcap.tikv.types.{DataType, DataTypeFactory, MySQLType}
-import com.pingcap.tikv.util.KeyRangeUtils
+import com.pingcap.tikv.types.DataType
 import com.pingcap.tispark.TiConfigConst
 import org.apache.spark.sql.SparkSession
 import org.slf4j.LoggerFactory
@@ -38,21 +33,21 @@ import org.slf4j.LoggerFactory
 import scala.collection.JavaConversions._
 import scala.collection.mutable
 
-private case class StatisticsDTO(colId: Long,
-                                 isIndex: Int,
-                                 distinct: Long,
-                                 version: Long,
-                                 nullCount: Long,
-                                 dataType: DataType,
-                                 rawCMSketch: Array[Byte],
-                                 idxInfo: TiIndexInfo,
-                                 colInfo: TiColumnInfo)
+private[statistics] case class StatisticsDTO(colId: Long,
+                                             isIndex: Int,
+                                             distinct: Long,
+                                             version: Long,
+                                             nullCount: Long,
+                                             dataType: DataType,
+                                             rawCMSketch: Array[Byte],
+                                             idxInfo: TiIndexInfo,
+                                             colInfo: TiColumnInfo)
 
-private case class StatisticsResult(histId: Long,
-                                    histogram: Histogram,
-                                    cMSketch: CMSketch,
-                                    idxInfo: TiIndexInfo,
-                                    colInfo: TiColumnInfo) {
+private[statistics] case class StatisticsResult(histId: Long,
+                                                histogram: Histogram,
+                                                cMSketch: CMSketch,
+                                                idxInfo: TiIndexInfo,
+                                                colInfo: TiColumnInfo) {
   def hasIdxInfo: Boolean = idxInfo != null
 
   def hasColInfo: Boolean = colInfo != null
@@ -60,29 +55,29 @@ private case class StatisticsResult(histId: Long,
 
 class StatisticsManager(tiSession: TiSession,
                         maxBktPerTbl: Long = Long.MaxValue,
-                        expireAfterAccess: Long = Long.MaxValue) {
+                        refreshAfterWrite: Long = Long.MaxValue) {
   private lazy val snapshot = tiSession.createSnapshot()
   private lazy val catalog = tiSession.getCatalog
   private lazy val metaTable = catalog.getTable("mysql", "stats_meta")
   private lazy val histTable = catalog.getTable("mysql", "stats_histograms")
   private lazy val bucketTable = catalog.getTable("mysql", "stats_buckets")
   private final lazy val logger = LoggerFactory.getLogger(getClass.getName)
+  private final val cacheLoader = new CacheLoader[Object, Object] {
+    override def load(tblIdObj: Object): Object = {
+      val tblId = tblIdObj.asInstanceOf[Long]
+      val tblStatistic = new TableStatistics(tblId)
+      null
+    }
+
+    override def reload(tableId: Object, tableStatistics: Object): ListenableFuture[Object] = {
+      null
+    }
+  }
+
   private final val statisticsMap = CacheBuilder
     .newBuilder()
-    .expireAfterAccess(expireAfterAccess, TimeUnit.MINUTES)
-    .maximumWeight(maxBktPerTbl) // cache should not grow beyond a certain size
-    .weigher(new Weigher[Object, Object] {
-      override def weigh(k: Object, v: Object): Int = {
-        def histSize(hist: Histogram): Int =
-          if (hist != null && hist.getBuckets != null) hist.getBuckets.size() else 0
-
-        // we calculate bucket number as weight. Weights are computed at entry creation time, and are static thereafter
-        val value = v.asInstanceOf[TableStatistics]
-        value.getColumnsHistMap.map(_._2.getHistogram).map(histSize).sum +
-          value.getIndexHistMap.map(_._2.getHistogram).map(histSize).sum
-      }
-    })
-    .build[Object, Object]
+    .refreshAfterWrite(refreshAfterWrite, TimeUnit.MINUTES)
+    .build(cacheLoader)
 
   def tableStatsFromStorage(table: TiTableInfo, columns: String*): Unit = synchronized {
     require(table != null, "TableInfo should not be null")
@@ -111,75 +106,14 @@ class StatisticsManager(tiSession: TiSession,
 
     // load count, modify_count, version info
     metaFromStorage(tblId, tblStatistic)
-
-    val req = new TiDAGRequest(PushDownType.NORMAL)
-    req.setTableInfo(histTable)
-    val start = RowKey.createMin(histTable.getId)
-    val end = RowKey.createBeyondMax(histTable.getId)
-    val ranges = mutable.ArrayBuffer[Coprocessor.KeyRange]()
-    ranges += KeyRangeUtils.makeCoprocRange(start.toByteString, end.toByteString)
-    req.addFilter(
-      ComparisonBinaryExpression
-        .equal(ColumnRef.create("table_id"), Constant.create(tblId))
-    )
-    req.addRequiredColumn(ColumnRef.create("table_id"))
-    req.addRequiredColumn(ColumnRef.create("is_index"))
-    req.addRequiredColumn(ColumnRef.create("hist_id"))
-    req.addRequiredColumn(ColumnRef.create("distinct_count"))
-    req.addRequiredColumn(ColumnRef.create("version"))
-    req.addRequiredColumn(ColumnRef.create("null_count"))
-    req.addRequiredColumn(ColumnRef.create("cm_sketch"))
-    req.addRanges(ranges)
-    req.setStartTs(snapshot.getTimestamp.getVersion)
-    req.resolve()
+    val req =
+      StatisticsHelper.buildHistogramsRequest(histTable, tblId, snapshot.getTimestamp.getVersion)
 
     val rows = snapshot.tableRead(req)
     if (!rows.hasNext) return
 
     val requests = rows
-      .map((row: Row) => {
-        val isIndex = if (row.getLong(1) > 0) true else false
-        val histID = row.getLong(2)
-        val distinct = row.getLong(3)
-        val histVer = row.getLong(4)
-        val nullCount = row.getLong(5)
-        val cMSketch = row.getBytes(6)
-        val indexInfos = table.getIndices.filter(_.getId == histID)
-        val colInfos = table.getColumns.filter(_.getId == histID)
-        var needed = true
-
-        // we should only query those columns that user specified before
-        if (!loadAll && !neededColIds.contains(histID)) needed = false
-
-        var indexFlag = 1
-        var dataType: DataType = DataTypeFactory.of(MySQLType.TypeBlob)
-        // Columns info found
-        if (!isIndex && colInfos.nonEmpty) {
-          indexFlag = 0
-          dataType = colInfos.head.getType
-        } else if (!isIndex || indexInfos.isEmpty) {
-          logger.error(
-            s"We cannot find histogram id $histID in table info ${table.getName} now. It may be deleted."
-          )
-          needed = false
-        }
-
-        if (needed) {
-          StatisticsDTO(
-            histID,
-            indexFlag,
-            distinct,
-            histVer,
-            nullCount,
-            dataType,
-            cMSketch,
-            if (indexInfos.nonEmpty) indexInfos.head else null,
-            if (colInfos.nonEmpty) colInfos.head else null
-          )
-        } else {
-          null
-        }
-      })
+      .map(StatisticsHelper.extractStatisticsDTO(_, table, loadAll, neededColIds))
       .filter(_ != null)
     val results = statisticsFromStorage(tblId, requests.toSeq)
 
@@ -207,23 +141,8 @@ class StatisticsManager(tiSession: TiSession,
   }
 
   private def metaFromStorage(tableId: Long, tableStatistics: TableStatistics): Unit = {
-    val req = new TiDAGRequest(PushDownType.NORMAL)
-    req.setTableInfo(metaTable)
-    val start = RowKey.createMin(metaTable.getId)
-    val end = RowKey.createBeyondMax(metaTable.getId)
-    val ranges = mutable.ArrayBuffer[Coprocessor.KeyRange]()
-    ranges += KeyRangeUtils.makeCoprocRange(start.toByteString, end.toByteString)
-    req.addFilter(
-      ComparisonBinaryExpression
-        .equal(ColumnRef.create("table_id"), Constant.create(tableId))
-    )
-    req.addRequiredColumn(ColumnRef.create("table_id"))
-    req.addRequiredColumn(ColumnRef.create("count"))
-    req.addRequiredColumn(ColumnRef.create("modify_count"))
-    req.addRequiredColumn(ColumnRef.create("version"))
-    req.addRanges(ranges)
-    req.setStartTs(snapshot.getTimestamp.getVersion)
-    req.resolve()
+    val req =
+      StatisticsHelper.buildMetaRequest(metaTable, tableId, snapshot.getTimestamp.getVersion)
 
     val rows = snapshot.tableRead(req)
     if (rows.isEmpty) return
@@ -236,29 +155,8 @@ class StatisticsManager(tiSession: TiSession,
 
   private def statisticsFromStorage(tableId: Long,
                                     requests: Seq[StatisticsDTO]): Seq[StatisticsResult] = {
-    val req = new TiDAGRequest(PushDownType.NORMAL)
-    req.setTableInfo(bucketTable)
-    val start = RowKey.createMin(bucketTable.getId)
-    val end = RowKey.createBeyondMax(bucketTable.getId)
-    val ranges = mutable.ArrayBuffer[Coprocessor.KeyRange]()
-    ranges += KeyRangeUtils.makeCoprocRange(start.toByteString, end.toByteString)
-    req.addFilter(
-      ComparisonBinaryExpression
-        .equal(ColumnRef.create("table_id"), Constant.create(tableId))
-    )
-    req.addOrderByItem(ByItem.create(ColumnRef.create("bucket_id"), false))
-    req.setLimit(Int.MaxValue)
-    req.addRequiredColumn(ColumnRef.create("count"))
-    req.addRequiredColumn(ColumnRef.create("repeats"))
-    req.addRequiredColumn(ColumnRef.create("lower_bound"))
-    req.addRequiredColumn(ColumnRef.create("upper_bound"))
-    req.addRequiredColumn(ColumnRef.create("bucket_id"))
-    req.addRequiredColumn(ColumnRef.create("table_id"))
-    req.addRequiredColumn(ColumnRef.create("is_index"))
-    req.addRequiredColumn(ColumnRef.create("hist_id"))
-    req.addRanges(ranges)
-    req.setStartTs(snapshot.getTimestamp.getVersion)
-    req.resolve()
+    val req =
+      StatisticsHelper.buildBucketRequest(bucketTable, tableId, snapshot.getTimestamp.getVersion)
 
     val rows = snapshot.tableRead(req)
     if (rows.isEmpty) return Nil
@@ -268,57 +166,7 @@ class StatisticsManager(tiSession: TiSession,
       .map((t: (Long, List[Row])) => {
         val histId = t._1
         val rows = t._2.iterator
-        val matches = requests.filter(_.colId == histId)
-        if (matches.nonEmpty) {
-          val matched = matches.head
-          var totalCount: Long = 0
-          val buckets = mutable.ArrayBuffer[Bucket]()
-          while (rows.hasNext) {
-            val row = rows.next()
-            val count = row.getLong(0)
-            val repeats = row.getLong(1)
-            var lowerBound: Key = null
-            var upperBound: Key = null
-            // all bounds are stored as blob in bucketTable currently, decode using blob type
-            lowerBound =
-              TypedKey.toTypedKey(row.getBytes(2), DataTypeFactory.of(MySQLType.TypeBlob))
-            upperBound =
-              TypedKey.toTypedKey(row.getBytes(3), DataTypeFactory.of(MySQLType.TypeBlob))
-            totalCount += count
-            buckets += new Bucket(totalCount, repeats, lowerBound, upperBound)
-          }
-          // create histogram for column `colId`
-          val histogram = Histogram
-            .newBuilder()
-            .setId(matched.colId)
-            .setNDV(matched.distinct)
-            .setNullCount(matched.nullCount)
-            .setLastUpdateVersion(matched.version)
-            .setBuckets(buckets)
-            .build()
-          // parse CMSketch
-          val rawData = matched.rawCMSketch
-          val cMSketch = if (rawData == null || rawData.length <= 0) {
-            null
-          } else {
-            val sketch = com.pingcap.tidb.tipb.CMSketch.parseFrom(rawData)
-            val result =
-              CMSketch.newCMSketch(sketch.getRowsCount, sketch.getRows(0).getCountersCount)
-            for (i <- 0 until sketch.getRowsCount) {
-              val row = sketch.getRows(i)
-              result.setCount(0)
-              for (j <- 0 until row.getCountersCount) {
-                val counter = row.getCounters(j)
-                result.getTable()(i)(j) = counter
-                result.setCount(result.getCount + counter)
-              }
-            }
-            result
-          }
-          StatisticsResult(histId, histogram, cMSketch, matched.idxInfo, matched.colInfo)
-        } else {
-          null
-        }
+        StatisticsHelper.extractStatisticResult(histId, rows, requests)
       })
       .filter(_ != null)
       .toSeq
