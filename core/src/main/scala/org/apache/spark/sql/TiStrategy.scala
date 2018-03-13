ef2d35c9c881c5e7c816feab6c9e49936fd8b0e7
Novemser <novemser@gmail.com>
2018-03-13 18:19:53 +0800

    Move logic

--- core/src/main/scala/org/apache/spark/sql/TiStrategy.scala
+++ core/src/main/scala/org/apache/spark/sql/TiStrategy.scala
@@ -257,12 +257,13 @@ class TiStrategy(context: SQLContext) extends Strategy with Logging {
     }
 
     dagRequest.addRanges(scanPlan.getKeyRanges)
-    if (!allowIndexDoubleRead())
-      scanPlan.getFilters.asScala.foreach { dagRequest.addFilter }
     if (scanPlan.isIndexScan) {
       dagRequest.setIndexInfo(scanPlan.getIndex)
       // need to set isDoubleRead to true for dagRequest in case of double read
       dagRequest.setIsDoubleRead(scanPlan.isDoubleRead)
+    }
+    if (!scanPlan.isDoubleRead) {
+      scanPlan.getFilters.asScala.foreach { dagRequest.addFilter }
     }
     dagRequest.setTableInfo(source.table)
     dagRequest
