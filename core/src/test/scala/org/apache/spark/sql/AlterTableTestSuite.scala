3b256673202c7057a178da35a0071b57b72d071e
birdstorm <samuelwyf@hotmail.com>
2018-04-16 21:32:07 +0800

    alter more types

--- core/src/test/scala/org/apache/spark/sql/AlterTableTestSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/AlterTableTestSuite.scala
@@ -15,12 +15,14 @@
 
 package org.apache.spark.sql
 
-/**
-  * Created by birdstorm on 2018/4/16.
-  */
 class AlterTableTestSuite extends BaseTiSparkSuite {
 
-  def alterTable(dataType: String, value: String, defaultVal: String, defaultVal2: String, nullable: Boolean = true, defaultNullOnly: Boolean = false): Unit = {
+  def alterTable(dataType: String,
+                 value: String,
+                 defaultVal: String,
+                 defaultVal2: String,
+                 nullable: Boolean = true,
+                 defaultNullOnly: Boolean = false): Unit = {
     tidbStmt.execute(s"drop table if exists t")
     tidbStmt.execute(s"create table t(c1 $dataType default $defaultVal)")
     tidbStmt.execute(s"insert into t values()")
@@ -42,14 +44,31 @@ class AlterTableTestSuite extends BaseTiSparkSuite {
       judge("select * from t")
     }
   }
+
   // https://github.com/pingcap/tikv-client-lib-java/issues/198
   test("Default value information not fetched") {
-    alterTable("varchar(45)", "a", "b", "c")
-    alterTable("datetime", "\"2018-01-01 00:00:00\"", "\"2018-01-01 11:11:11\"", "\"2018-01-01 22:22:22\"")
+    alterTable("varchar(45)", "\"a\"", "\"b\"", "\"c\"")
+    alterTable(
+      "datetime",
+      "\"2018-01-01 00:00:00\"",
+      "\"2018-01-01 11:11:11\"",
+      "\"2018-01-01 22:22:22\""
+    )
     alterTable("date", "\"2018-01-01\"", "\"2018-02-02\"", "\"2018-03-03\"")
-    alterTable("timestamp", "\"2018-01-01 00:00:00\"", "\"2018-01-01 11:11:11\"", "\"2018-01-01 22:22:22\"", nullable = false)
+    alterTable(
+      "timestamp",
+      "\"2018-01-01 00:00:00\"",
+      "\"2018-01-01 11:11:11\"",
+      "\"2018-01-01 22:22:22\"",
+      nullable = false
+    )
     alterTable("bigint", "9223372036854775807", "-9223372036854775808", "1")
-    alterTable("decimal", "1.7976931348623157E308", "0.7976931348623157E308", "-0.7976931348623157E308")
+    alterTable(
+      "decimal",
+      "1.7976931348623157E308",
+      "0.7976931348623157E308",
+      "-0.7976931348623157E308"
+    )
     alterTable("double", "0.1", "1.2", "3.4")
     alterTable("float", "0.1", "1.2", "3.4")
     alterTable("int", "0", "1", "3")
@@ -57,10 +76,10 @@ class AlterTableTestSuite extends BaseTiSparkSuite {
     alterTable("real", "0.1", "1.2", "3.4")
     alterTable("smallint", "0", "1", "3")
     alterTable("tinyint", "b'0'", "b'1'", "b'0'")
-    alterTable("char(10)", "a", "b", "c")
-    alterTable("varchar(40)", "a", "b", "c")
-    alterTable("text", "0", "null", "", defaultNullOnly = true)
-    alterTable("blob", "0", "null", "", defaultNullOnly = true)
+    alterTable("char(10)", "\"a\"", "\"b\"", "\"c\"")
+    alterTable("varchar(40)", "\"a\"", "\"b\"", "\"c\"")
+    alterTable("text", "\"0\"", "null", "", defaultNullOnly = true)
+    alterTable("blob", "\"0\"", "null", "", defaultNullOnly = true)
   }
 
   override def afterAll(): Unit = {
