c5435bc9ce2e2db9d31b8705c2e25b4d317e4be7
Soup <maxiaoyu@pingcap.com>
2018-07-05 23:22:00 +0800

    Fix scan iterator (#383)

--- tikv-client/src/main/java/com/pingcap/tikv/operation/iterator/ScanIterator.java
+++ tikv-client/src/main/java/com/pingcap/tikv/operation/iterator/ScanIterator.java
@@ -17,11 +17,10 @@ package com.pingcap.tikv.operation.iterator;
 
 import static java.util.Objects.requireNonNull;
 
-import com.google.common.collect.Range;
 import com.google.protobuf.ByteString;
 import com.pingcap.tikv.TiSession;
 import com.pingcap.tikv.exception.TiClientInternalException;
-import com.pingcap.tikv.kvproto.Coprocessor.KeyRange;
+import com.pingcap.tikv.key.Key;
 import com.pingcap.tikv.kvproto.Kvrpcpb;
 import com.pingcap.tikv.kvproto.Metapb;
 import com.pingcap.tikv.region.RegionManager;
@@ -29,16 +28,11 @@ import com.pingcap.tikv.region.RegionStoreClient;
 import com.pingcap.tikv.region.TiRegion;
 import com.pingcap.tikv.util.BackOffer;
 import com.pingcap.tikv.util.ConcreteBackOffer;
-import com.pingcap.tikv.util.KeyRangeUtils;
 import com.pingcap.tikv.util.Pair;
-import com.pingcap.tikv.key.Key;
 import java.util.Iterator;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 public class ScanIterator implements Iterator<Kvrpcpb.KvPair> {
-  private final Range<Key> scanRange;
-  private final int batchSize;
   protected final TiSession session;
   private final RegionManager regionCache;
   protected final long version;
@@ -46,30 +40,29 @@ public class ScanIterator implements Iterator<Kvrpcpb.KvPair> {
   private List<Kvrpcpb.KvPair> currentCache;
   protected ByteString startKey;
   protected int index = -1;
-  private boolean endOfRegion = false;
+  private boolean endOfScan = false;
 
   public ScanIterator(
       ByteString startKey,
-      int batchSize,
-      KeyRange range,
       TiSession session,
-      RegionManager rm,
       long version) {
     this.startKey = requireNonNull(startKey, "start key is null");
-    this.batchSize = batchSize;
-    if (range == null) {
-      this.scanRange = Range.all();
-    } else {
-      this.scanRange = KeyRangeUtils.makeRange(range.getStart(), range.getEnd());
+    if (startKey.isEmpty()) {
+      throw new IllegalArgumentException("start key cannot be empty");
     }
     this.session = session;
-    this.regionCache = rm;
+    this.regionCache = session.getRegionManager();
     this.version = version;
   }
 
+  // return false if current cache is not loaded or empty
   private boolean loadCache() {
-    if (endOfRegion) return false;
-
+    if (endOfScan) {
+      return false;
+    }
+    if (startKey.isEmpty()) {
+      return false;
+    }
     Pair<TiRegion, Metapb.Store> pair = regionCache.getRegionStorePairByKey(startKey);
     TiRegion region = pair.first;
     Metapb.Store store = pair.second;
@@ -83,24 +76,21 @@ public class ScanIterator implements Iterator<Kvrpcpb.KvPair> {
       // Session should be single-threaded itself
       // so that we don't worry about conf change in the middle
       // of a transaction. Otherwise below code might lose data
-      if (currentCache.size() < batchSize) {
+      if (currentCache.size() < session.getConf().getScanBatchSize()) {
         // Current region done, start new batch from next region
         startKey = region.getEndKey();
-        if (startKey.size() == 0 || !contains(startKey)) {
-          return false;
-        }
       } else {
         // Start new scan from exact next key in current region
         Key lastKey = Key.toRawKey(currentCache.get(currentCache.size() - 1).getKey());
         startKey = lastKey.next().toByteString();
       }
     } catch (Exception e) {
-      throw new TiClientInternalException("Error Closing Store client.", e);
+      throw new TiClientInternalException("Error scanning data from region.", e);
     }
     return true;
   }
 
-  private boolean cacheDrain() {
+  private boolean isCacheDrained() {
     return currentCache == null
         || index >= currentCache.size()
         || index == -1;
@@ -108,39 +98,21 @@ public class ScanIterator implements Iterator<Kvrpcpb.KvPair> {
 
   @Override
   public boolean hasNext() {
-    if (cacheDrain() && endOfRegion) {
-      return false;
-    }
-    if (cacheDrain()) {
-      if (!loadCache()) {
-        endOfRegion = true;
-      }
-    }
-    if (!cacheDrain() && !contains(currentCache.get(index).getKey())) {
-      endOfRegion = true;
+    if (isCacheDrained() && !loadCache()) {
+      endOfScan = true;
       return false;
     }
     return true;
   }
 
   private Kvrpcpb.KvPair getCurrent() {
-    if (cacheDrain() && endOfRegion) {
-      throw new NoSuchElementException();
+    if (isCacheDrained()) {
+      return null;
     }
     if (index < currentCache.size()) {
-      Kvrpcpb.KvPair kv = currentCache.get(index++);
-      if (!contains(kv.getKey())) {
-        endOfRegion = true;
-        throw new NoSuchElementException();
-      }
-      return kv;
+      return currentCache.get(index++);
     }
     return null;
-  }
-
-
-  private boolean contains(ByteString key) {
-    return scanRange.contains(Key.toRawKey(key));
   }
 
   @Override
