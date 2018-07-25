7579124d27f3dc5520e82471f1838b3cbb3b7c15
birdstorm <samuelwyf@hotmail.com>
2018-07-25 12:17:55 +0800

    Set IgnoreUnknown=true for JsonCreator (#405)

--- tikv-client/src/main/java/com/pingcap/tikv/meta/TiDBInfo.java
+++ tikv-client/src/main/java/com/pingcap/tikv/meta/TiDBInfo.java
@@ -16,9 +16,11 @@
 package com.pingcap.tikv.meta;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TiDBInfo {
   private long id;
   private String name;
