82289313f7c06bb6c4536db54b7bd1d7390c0fb0
birdstorm <samuelwyf@hotmail.com>
2018-10-12 22:16:56 +0800

    Use case class for execution commands (#458)

--- core/src/main/scala/org/apache/spark/sql/execution/command/TiCommand.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/command/TiCommand.scala
@@ -1,9 +1,20 @@
 package org.apache.spark.sql.execution.command
 
-import org.apache.spark.sql.TiContext
+import org.apache.spark.sql.{Row, SparkSession, TiContext}
 import org.apache.spark.sql.catalyst.catalog.TiSessionCatalog
+import org.apache.spark.sql.catalyst.expressions.Attribute
+import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 
-trait TiCommand {
+/**
+ * TiCommand is used to inherit from [[org.apache.spark.sql.execution.command.RunnableCommand]]
+ *
+ * Because we are unable to extend from a case class implementing RunnableCommand, we will have
+ * to extend TiCommand from its abstract class directly.
+ */
+abstract class TiCommand(delegate: RunnableCommand) extends RunnableCommand {
   val tiContext: TiContext
   def tiCatalog: TiSessionCatalog = tiContext.tiCatalog
+  override def output: Seq[Attribute] = delegate.output
+  override def children: Seq[LogicalPlan] = delegate.children
+  override def run(sparkSession: SparkSession): Seq[Row] = delegate.run(sparkSession)
 }
