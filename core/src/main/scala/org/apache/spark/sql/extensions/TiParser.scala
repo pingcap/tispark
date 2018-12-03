b36053141d6d1f909069d5a35fe92d44d598a485
birdstorm <samuelwyf@hotmail.com>
2018-12-04 01:40:09 +0800

    Fix unresolved column name when using table.column (#512)

--- core/src/main/scala/org/apache/spark/sql/extensions/TiParser.scala
+++ core/src/main/scala/org/apache/spark/sql/extensions/TiParser.scala
@@ -1,6 +1,7 @@
 package org.apache.spark.sql.extensions
 
 import org.apache.spark.sql.catalyst.analysis.UnresolvedRelation
+import org.apache.spark.sql.catalyst.catalog.TiSessionCatalog
 import org.apache.spark.sql.catalyst.expressions.{Exists, Expression, ListQuery, NamedExpression, ScalarSubquery}
 import org.apache.spark.sql.catalyst.parser._
 import org.apache.spark.sql.catalyst.plans.logical._
@@ -14,7 +15,7 @@ case class TiParser(getOrCreateTiContext: SparkSession => TiContext)(sparkSessio
                                                                      delegate: ParserInterface)
     extends ParserInterface {
   private lazy val tiContext = getOrCreateTiContext(sparkSession)
-
+  private lazy val tiCatalog = tiContext.tiCatalog
   private lazy val internal = new SparkSqlParser(sparkSession.sqlContext.conf)
 
   private def qualifyTableIdentifierInternal(tableIdentifier: TableIdentifier): TableIdentifier =
@@ -23,59 +24,72 @@ case class TiParser(getOrCreateTiContext: SparkSession => TiContext)(sparkSessio
       Some(tableIdentifier.database.getOrElse(tiContext.tiCatalog.getCurrentDatabase))
     )
 
+  /**
+   * Determines whether a table specified by tableIdentifier is
+   * NOT a tempView registered. This is used for TiSpark to transform
+   * plans and decides whether a relation should be resolved or parsed.
+   *
+   * @param tableIdentifier tableIdentifier
+   * @return whether it is not a tempView
+   */
   private def notTempView(tableIdentifier: TableIdentifier) =
-    tableIdentifier.database.isEmpty && tiContext.sessionCatalog
-      .getTempView(tableIdentifier.table)
-      .isEmpty
+    !tiCatalog.isTemporaryTable(tableIdentifier)
 
   /**
    * WAR to lead Spark to consider this relation being on local files.
    * Otherwise Spark will lookup this relation in his session catalog.
    * See [[org.apache.spark.sql.catalyst.analysis.Analyzer.ResolveRelations.resolveRelation]] for detail.
+   *
+   * Here we use transformUp when transforming logicalPlans because We should first
+   * deal with the leaf nodes, and a bottom-to-top recursion is needed.
    */
   private val qualifyTableIdentifier: PartialFunction[LogicalPlan, LogicalPlan] = {
     case r @ UnresolvedRelation(tableIdentifier)
-        if tableIdentifier.database.isEmpty && notTempView(tableIdentifier) =>
-      r.copy(qualifyTableIdentifierInternal(tableIdentifier))
+        if tiCatalog
+          .catalogOf(tableIdentifier.database)
+          .exists(_.isInstanceOf[TiSessionCatalog]) && notTempView(tableIdentifier) =>
+      // Use SubqueryAlias so that projects and joins can correctly resolve
+      // UnresolvedAttributes in JoinConditions, Projects, Filters, etc.
+      SubqueryAlias(tableIdentifier.table, r.copy(qualifyTableIdentifierInternal(tableIdentifier)))
     case f @ Filter(condition, _) =>
       f.copy(
-        condition = condition.transform {
-          case e @ Exists(plan, _, _) => e.copy(plan = plan.transform(qualifyTableIdentifier))
+        condition = condition.transformUp {
+          case e @ Exists(plan, _, _) => e.copy(plan = plan.transformUp(qualifyTableIdentifier))
           case ls @ ListQuery(plan, _, _, _) =>
-            ls.copy(plan = plan.transform(qualifyTableIdentifier))
+            ls.copy(plan = plan.transformUp(qualifyTableIdentifier))
           case s @ ScalarSubquery(plan, _, _) =>
-            s.copy(plan = plan.transform(qualifyTableIdentifier))
+            s.copy(plan = plan.transformUp(qualifyTableIdentifier))
         }
       )
     case p @ Project(projectList, _) =>
       p.copy(
-        projectList = projectList.map(_.transform {
+        projectList = projectList.map(_.transformUp {
           case s @ ScalarSubquery(plan, _, _) =>
-            s.copy(plan = plan.transform(qualifyTableIdentifier))
+            s.copy(plan = plan.transformUp(qualifyTableIdentifier))
         }.asInstanceOf[NamedExpression])
       )
     case w @ With(_, cteRelations) =>
       w.copy(
         cteRelations = cteRelations
-          .map(p => (p._1, p._2.transform(qualifyTableIdentifier).asInstanceOf[SubqueryAlias]))
+          .map(p => (p._1, p._2.transformUp(qualifyTableIdentifier).asInstanceOf[SubqueryAlias]))
       )
     case cv @ CreateViewCommand(_, _, _, _, _, child, _, _, _) =>
-      cv.copy(child = child.transform(qualifyTableIdentifier))
+      cv.copy(child = child.transformUp(qualifyTableIdentifier))
     case e @ ExplainCommand(logicalPlan, _, _, _) =>
-      e.copy(logicalPlan = logicalPlan.transform(qualifyTableIdentifier))
+      e.copy(logicalPlan = logicalPlan.transformUp(qualifyTableIdentifier))
     case c @ CacheTableCommand(tableIdentifier, plan, _)
         if plan.isEmpty && notTempView(tableIdentifier) =>
       // Caching an unqualified catalog table.
       c.copy(qualifyTableIdentifierInternal(tableIdentifier))
     case c @ CacheTableCommand(_, plan, _) if plan.isDefined =>
-      c.copy(plan = Some(plan.get.transform(qualifyTableIdentifier)))
+      c.copy(plan = Some(plan.get.transformUp(qualifyTableIdentifier)))
     case u @ UncacheTableCommand(tableIdentifier, _) if notTempView(tableIdentifier) =>
       // Uncaching an unqualified catalog table.
       u.copy(qualifyTableIdentifierInternal(tableIdentifier))
   }
 
   override def parsePlan(sqlText: String): LogicalPlan =
-    internal.parsePlan(sqlText).transform(qualifyTableIdentifier)
+    internal.parsePlan(sqlText).transformUp(qualifyTableIdentifier)
 
   override def parseExpression(sqlText: String): Expression =
     internal.parseExpression(sqlText)
