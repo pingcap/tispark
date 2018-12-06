8cf580b03d21a3222e5ff1bb99a35376c0db79e1
birdstorm <samuelwyf@hotmail.com>
2018-12-06 17:11:02 +0800

    support extended and formatted describe table command

--- core/src/main/scala/org/apache/spark/sql/execution/command/tables.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/command/tables.scala
@@ -16,7 +16,8 @@ package org.apache.spark.sql.execution.command
 
 import org.apache.spark.sql.catalyst.analysis.NoSuchDatabaseException
 import org.apache.spark.sql.catalyst.catalog.{CatalogTable, SessionCatalog, TiSessionCatalog}
-import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.catalyst.expressions.{Attribute, AttributeReference}
+import org.apache.spark.sql.types.{MetadataBuilder, StringType, StructType}
 import org.apache.spark.sql.{AnalysisException, Row, SparkSession, TiContext}
 
 import scala.collection.mutable.ArrayBuffer
@@ -46,6 +47,33 @@ case class TiShowTablesCommand(tiContext: TiContext, delegate: ShowTablesCommand
 
 case class TiDescribeTablesCommand(tiContext: TiContext, delegate: DescribeTableCommand)
     extends TiCommand(delegate) {
+  override val output: Seq[Attribute] = Seq(
+    // Column names are based on Hive.
+    AttributeReference(
+      "col_name",
+      StringType,
+      nullable = false,
+      new MetadataBuilder().putString("comment", "name of the column").build()
+    )(),
+    AttributeReference(
+      "data_type",
+      StringType,
+      nullable = false,
+      new MetadataBuilder().putString("comment", "data type of the column").build()
+    )(),
+    AttributeReference(
+      "nullable",
+      StringType,
+      nullable = false,
+      new MetadataBuilder().putString("comment", "whether the column is nullable").build()
+    )(),
+    AttributeReference(
+      "comment",
+      StringType,
+      nullable = true,
+      new MetadataBuilder().putString("comment", "comment of the column").build()
+    )()
+  )
   override def run(sparkSession: SparkSession): Seq[Row] =
     tiCatalog
       .catalogOf(delegate.table.database)
@@ -58,7 +86,7 @@ case class TiDescribeTablesCommand(tiContext: TiContext, delegate: DescribeTable
         val result = new ArrayBuffer[Row]
         if (delegate.partitionSpec.nonEmpty) {
           throw new AnalysisException(
-            s"DESC PARTITION is not allowed on Flash table: ${delegate.table.identifier}"
+            s"DESC PARTITION is not supported on TiDB table: ${delegate.table.identifier}"
           )
         }
         val metadata = tiCatalog.getTableMetadata(delegate.table)
@@ -79,21 +107,31 @@ case class TiDescribeTablesCommand(tiContext: TiContext, delegate: DescribeTable
       append(buffer, s"# ${output.head.name}", output(1).name, output(2).name)
     }
     schema.foreach { column =>
-      append(buffer, column.name, column.dataType.simpleString, column.getComment().orNull)
+      append(
+        buffer,
+        column.name,
+        column.dataType.simpleString,
+        column.getComment().orNull,
+        column.nullable.toString
+      )
     }
   }
 
   private def append(buffer: ArrayBuffer[Row],
                      column: String,
                      dataType: String,
-                     comment: String): Unit =
-    buffer += Row(column, dataType, comment)
+                     comment: String,
+                     nullable: String = ""): Unit =
+    buffer += Row(column, dataType, nullable, comment)
 
   private def describeFormattedTableInfo(table: CatalogTable, buffer: ArrayBuffer[Row]): Unit = {
     // So far we only have engine name and primary key for extended information.
     // TODO: Add more extended table information.
     append(buffer, "", "", "")
     append(buffer, "# Detailed Table Information", "", "")
+    table.toLinkedHashMap.foreach { s =>
+      append(buffer, s._1, s._2, "")
+    }
   }
 
 }
