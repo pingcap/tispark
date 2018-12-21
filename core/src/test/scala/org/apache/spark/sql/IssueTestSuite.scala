3ce60fb6adb3371ea8dbfc3903ab559194a27704
zhexuany <yangzhexuan@pingcap.com>
2018-12-21 18:49:21 +0800

    adding refreshConnection

--- core/src/test/scala/org/apache/spark/sql/IssueTestSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/IssueTestSuite.scala
@@ -29,8 +29,6 @@ class IssueTestSuite extends BaseTiSparkSuite {
     tidbStmt.execute("INSERT INTO set_t(priority) VALUES('High')")
     tidbStmt.execute("INSERT INTO set_t(priority) VALUES('Medium')")
     tidbStmt.execute("INSERT INTO set_t(priority) VALUES('Low')")
-    judge("select * from set_t")
-    judge("select * from set_t where priority = 'High'")
     tidbStmt.execute(
       "CREATE TABLE `enum_t` (" +
         "`priority` set('Low','Medium','High') NOT NULL)"
@@ -38,6 +36,9 @@ class IssueTestSuite extends BaseTiSparkSuite {
     tidbStmt.execute("INSERT INTO enum_t(priority) VALUES('High')")
     tidbStmt.execute("INSERT INTO enum_t(priority) VALUES('Medium')")
     tidbStmt.execute("INSERT INTO enum_t(priority) VALUES('Low')")
+    refreshConnections()
+    judge("select * from set_t")
+    judge("select * from set_t where priority = 'High'")
     judge("select * from enum_t")
     judge("select * from enum_t where priority = 'High'")
   }
