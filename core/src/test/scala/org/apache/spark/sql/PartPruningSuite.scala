ca9faebe6001b19d3786c3ba9266b89a4346f7a7
zhexuany <yangzhexuan@pingcap.com>
2018-12-29 21:56:04 +0800

    adding index test

--- core/src/test/scala/org/apache/spark/sql/PartPruningSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/PartPruningSuite.scala
@@ -1,17 +1,126 @@
 package org.apache.spark.sql
 
-import com.pingcap.tikv.meta.{TiDAGRequest, TiPartitionDef}
-import org.apache.spark.sql.execution.CoprocessorRDD
+import com.pingcap.tikv.meta.TiDAGRequest
+import org.apache.spark.sql.execution.{CoprocessorRDD, RegionTaskExec}
 
 // NOTE: when you create new table, remember drop them at after all.
 class PartPruningSuite extends BaseTiSparkSuite {
   private def extractDAGReq(df: DataFrame): TiDAGRequest =
-    df.queryExecution.executedPlan
-      .find(e => e.isInstanceOf[CoprocessorRDD])
-      .get
-      .asInstanceOf[CoprocessorRDD]
-      .tiRdd
-      .dagRequest
+    if (!df.queryExecution.executedPlan
+          .find(e => e.isInstanceOf[CoprocessorRDD])
+          .isEmpty) {
+      df.queryExecution.executedPlan
+        .find(e => e.isInstanceOf[CoprocessorRDD])
+        .get
+        .asInstanceOf[CoprocessorRDD]
+        .tiRdd
+        .dagRequest
+    } else {
+      df.queryExecution.executedPlan
+        .find(e => e.isInstanceOf[RegionTaskExec])
+        .get
+        .asInstanceOf[RegionTaskExec]
+        .dagRequest
+    }
+
+  test("adding part pruning test when index is on partitioned column") {
+    tidbStmt.execute("DROP TABLE IF EXISTS `partition_t`")
+    tidbStmt.execute("""
+                       |CREATE TABLE `partition_t` (
+                       |  `id` int(11) DEFAULT NULL,
+                       |  `name` varchar(50) DEFAULT NULL,
+                       |  `purchased` date DEFAULT NULL,
+                       |  index `idx_pur`(`purchased`)
+                       |) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+                       |PARTITION BY RANGE ( weekday(purchased) ) (
+                       |  PARTITION p0 VALUES LESS THAN (2),
+                       |  PARTITION p1 VALUES LESS THAN (4),
+                       |  PARTITION p2 VALUES LESS THAN (6)
+                       |)
+                     """.stripMargin)
+    refreshConnections()
+    assert(
+      extractDAGReq(
+        spark.sql("select * from partition_t")
+      ).getPartInfo.getDefs
+        .size() == 3
+    )
+    assert(
+      extractDAGReq(
+        spark
+        // select weekday('1998-10-10') is 5
+        // expected part info only contains one part which is p2.
+          .sql("select * from partition_t where purchased = date'1998-10-10'")
+      ).getPartInfo.getDefs
+        .get(0)
+        .getName == "p2"
+    )
+
+    assert(
+      extractDAGReq(
+        spark
+        // select weekday('1998-10-10') is 5
+        // expected part info only contains one part which is p2.
+          .sql("select * from partition_t where purchased > date'1998-10-10'")
+      ).getPartInfo.getDefs
+        .get(0)
+        .getName == "p2"
+    )
+
+    assert(
+      {
+        val pDef = extractDAGReq(
+          spark
+          // select weekday('1998-10-09') is 4
+          // expected part info only contains two parts which are p0 and p1.
+            .sql("select * from partition_t where purchased < date'1998-10-09'")
+        ).getPartInfo.getDefs
+        pDef.size() == 2 && pDef.get(0).getName == "p0" && pDef.get(1).getName == "p1"
+      }
+    )
+
+    assert(
+      {
+        val pDef = extractDAGReq(
+          spark
+          // select weekday('1998-10-09') is 4 and select weekday('1998-10-08') is 3
+          // expected part info only contains one part which is p1.
+            .sql(
+              "select * from partition_t where purchased < date'1998-10-09' and purchased > date'1998-10-08'"
+            )
+        ).getPartInfo.getDefs
+        pDef.size() == 1 && pDef.get(0).getName == "p1"
+      }
+    )
+
+    assert(
+      {
+        val pDef = extractDAGReq(
+          spark
+          // select weekday('1998-10-09') is 4 and select weekday('1998-10-07') is 2
+          // expected parts info only contain two parts which is p0 and p2.
+            .sql(
+              "select * from partition_t where purchased < date'1998-10-07' or purchased > date'1998-10-09'"
+            )
+        ).getPartInfo.getDefs
+        pDef.size() == 2 && pDef.get(0).getName == "p0" && pDef.get(1).getName == "p2"
+      }
+    )
+
+    assert(
+      {
+        val pDef = extractDAGReq(
+          spark
+          // select weekday('1998-10-09') is 4 and select weekday('1998-10-07') is 2
+          // expected part info only contain one part which is p1.
+            .sql(
+              "select * from partition_t where purchased > date'1998-10-07' and purchased < date'1998-10-09'"
+            )
+        ).getPartInfo.getDefs
+        pDef.size() == 1 && pDef.get(0).getName == "p1"
+      }
+    )
+  }
 
   test("adding part pruning test") {
     tidbStmt.execute("DROP TABLE IF EXISTS `partition_t`")
