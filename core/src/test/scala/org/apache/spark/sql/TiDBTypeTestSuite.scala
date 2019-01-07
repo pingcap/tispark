19fd18e738f78aa943060be3e897475292b0b4ed
zhexuany <yangzhexuan@pingcap.com>
2019-01-07 17:42:50 +0800

    move json to type test

--- core/src/test/scala/org/apache/spark/sql/TiDBTypeTestSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/TiDBTypeTestSuite.scala
@@ -55,10 +55,10 @@ class TiDBTypeTestSuite extends BaseTiSparkSuite {
       " CREATE TABLE `set_t` (\n  `col` " +
         "set('1','2','3','4', '5', '6','7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29','30','31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49','50','51', '52', '53', '54', '55', '56','57', '58', '59', '60', '61', '62', '63', '64')\n)"
     )
-//    tidbStmt.execute("INSERT INTO set_t(col) VALUES('1')")
-    //    tidbStmt.execute("INSERT INTO set_t(col) VALUES('1,3')"
-//    tidbStmt.execute("INSERT INTO set_t(col) VALUES('1,32')")
-//    tidbStmt.execute("INSERT INTO set_t(col) VALUES('1,63')")
+    tidbStmt.execute("INSERT INTO set_t(col) VALUES('1')")
+    tidbStmt.execute("INSERT INTO set_t(col) VALUES('1,3')")
+    tidbStmt.execute("INSERT INTO set_t(col) VALUES('1,32')")
+    tidbStmt.execute("INSERT INTO set_t(col) VALUES('1,63')")
     tidbStmt.execute("INSERT INTO set_t(col) VALUES('1,64')")
     tidbStmt.execute(
       "CREATE TABLE `enum_t` (" +
@@ -74,12 +74,75 @@ class TiDBTypeTestSuite extends BaseTiSparkSuite {
     judge("select * from enum_t where priority = 'High'")
   }
 
+
+test("adding json support") {
+    tidbStmt.execute("drop table if exists t")
+    tidbStmt.execute("create table t(json_doc json)")
+    tidbStmt.execute(
+      """insert into t values  ('null'),
+          ('true'),
+          ('false'),
+          ('0'),
+          ('1'),
+          ('-1'),
+          ('2147483647'),
+          ('-2147483648'),
+          ('9223372036854775807'),
+          ('-9223372036854775808'),
+          ('0.5'),
+          ('-0.5'),
+          ('""'),
+          ('"a"'),
+          ('"\\t"'),
+          ('"\\n"'),
+          ('"\\""'),
+          ('"\\u0001"'),
+          ('[]'),
+          ('"中文"'),
+          (JSON_ARRAY(null, false, true, 0, 0.5, "hello", JSON_ARRAY("nested_array"), JSON_OBJECT("nested", "object"))),
+          (JSON_OBJECT("a", null, "b", true, "c", false, "d", 0, "e", 0.5, "f", "hello", "nested_array", JSON_ARRAY(1, 2, 3), "nested_object", JSON_OBJECT("hello", 1)))"""
+    )
+    refreshConnections()
+
+    runTest(
+      "select json_doc from t",
+      skipJDBC = true,
+      rTiDB = List(
+        List("null"),
+        List(true),
+        List(false),
+        List(0),
+        List(1),
+        List(-1),
+        List(2147483647),
+        List(-2147483648),
+        List(9223372036854775807L),
+        List(-9223372036854775808L),
+        List(0.5),
+        List(-0.5),
+        List("\"\""),
+        List("\"a\""),
+        List("\"\\t\""),
+        List("\"\\n\""),
+        List("\"\\\"\""),
+        List("\"\\u0001\""),
+        List("[]"),
+        List("\"中文\""),
+        List("[null,false,true,0,0.5,\"hello\",[\"nested_array\"],{\"nested\":\"object\"}]"),
+        List(
+          "{\"a\":null,\"b\":true,\"c\":false,\"d\":0,\"e\":0.5,\"f\":\"hello\",\"nested_array\":[1,2,3],\"nested_object\":{\"hello\":1}}"
+        )
+      )
+    )
+  }
+
   override def afterAll(): Unit =
     try {
       tidbStmt.execute("drop table if exists set_t")
       tidbStmt.execute("drop table if exists enum_t")
       tidbStmt.execute("drop table if exists t_t")
       tidbStmt.execute("drop table if exists y_t")
+      tidbStmt.execute("drop table if exists t")
     } finally {
       super.afterAll()
     }
