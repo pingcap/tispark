4ff1541d826eec8eeb1dcd10a74645d1dc87c5b0
birdstorm <samuelwyf@hotmail.com>
2019-04-29 14:22:57 +0800

    change access level of TiSession.getPDClient() (#688)

--- tikv-client/src/main/java/com/pingcap/tikv/TiSession.java
+++ tikv-client/src/main/java/com/pingcap/tikv/TiSession.java
@@ -63,7 +63,7 @@ public class TiSession implements AutoCloseable {
     return new Snapshot(ts, this);
   }
 
-  PDClient getPDClient() {
+  public PDClient getPDClient() {
     PDClient res = client;
     if (res == null) {
       synchronized (this) {
