f9eebe719c76e598e013bf2cb732af854e512eab
zhexuany <yangzhexuan@pingcap.com>
2019-05-08 20:23:21 +0800

    adding comments

--- core/src/test/scala/com/pingcap/tispark/datasource/AllocatorSuite.scala
+++ core/src/test/scala/com/pingcap/tispark/datasource/AllocatorSuite.scala
@@ -1,10 +1,28 @@
 package com.pingcap.tispark.datasource
 
 import com.pingcap.tikv.IDAllocator
-import org.apache.spark.sql.{BaseTiSparkSuite, TiStrategy}
+import org.apache.spark.sql.BaseTiSparkSuite
 
 class AllocatorSuite extends BaseTiSparkSuite {
-  test("test allocator") {
+  test("test unsigned allocator") {
+    tidbStmt.execute("drop table if exists t")
+    tidbStmt.execute("""CREATE TABLE `t` (
+                       |  `a` int(11) DEFAULT NULL
+                       |) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+      """.stripMargin)
+    refreshConnections()
+    val dbName = "tidb_tispark_test"
+    val tableName = "t"
+    val tiDBInfo = ti.tiSession.getCatalog.getDatabase(dbName)
+    val tiTableInfo =
+      ti.tiSession.getCatalog.getTable(dbName, tableName)
+    // corner case allocate unsigned long's max value.
+    val allocator = new IDAllocator(tiDBInfo.getId, ti.tiSession.getCatalog, true, -1L)
+    allocator.alloc(tiTableInfo.getId)
+    assert(allocator.getEnd - allocator.getStart == -2L)
+  }
+
+  test("test signed allocator") {
     tidbStmt.execute("drop table if exists t")
     tidbStmt.execute("""CREATE TABLE `t` (
                        |  `a` int(11) DEFAULT NULL
