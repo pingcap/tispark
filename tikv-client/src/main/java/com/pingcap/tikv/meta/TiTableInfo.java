181944ff101bcc950d50cbed28e55e7fbed62084
marsishandsome <marsishandsome@gmail.com>
2019-05-13 17:47:14 +0800

    refactor hasPrimaryKey

--- tikv-client/src/main/java/com/pingcap/tikv/meta/TiTableInfo.java
+++ tikv-client/src/main/java/com/pingcap/tikv/meta/TiTableInfo.java
@@ -47,6 +47,8 @@ public class TiTableInfo implements Serializable {
   private final long columnLength;
   private final TiPartitionInfo partitionInfo;
 
+  private final TiColumnInfo primaryKeyColumn;
+
   @JsonCreator
   public TiTableInfo(
       @JsonProperty("id") long id,
@@ -78,6 +80,15 @@ public class TiTableInfo implements Serializable {
     this.maxIndexId = maxIndexId;
     this.oldSchemaId = oldSchemaId;
     this.partitionInfo = partitionInfo;
+
+    TiColumnInfo primaryKey = null;
+    for (TiColumnInfo col : this.columns) {
+      if (col.isPrimaryKey()) {
+        primaryKey = col;
+        break;
+      }
+    }
+    primaryKeyColumn = primaryKey;
   }
 
   public long getId() {
@@ -156,12 +167,7 @@ public class TiTableInfo implements Serializable {
   }
 
   public boolean hasPrimaryKey() {
-    for (TiColumnInfo col : getColumns()) {
-      if (col.isPrimaryKey()) {
-        return true;
-      }
-    }
-    return false;
+    return primaryKeyColumn != null;
   }
 
   // Only Integer Column will be a PK column
