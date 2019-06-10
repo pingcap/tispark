58c67800812c12e0087ab2b430039a1f2fca42f0
zhexuany <yangzhexuan@pingcap.com>
2019-06-10 12:20:06 +0800

    fix collection is empty during region pre split

--- core/src/main/scala/com/pingcap/tispark/TiBatchWrite.scala
+++ core/src/main/scala/com/pingcap/tispark/TiBatchWrite.scala
@@ -253,8 +253,9 @@ class TiBatchWrite(@transient val df: DataFrame,
       val sampleRDD =
         rdd.sample(withReplacement = false, fraction = options.sampleFraction)
       val (dataSize, sampleRDDCount) = estimateDataSize(sampleRDD, options)
-
-      tiContext.tiSession.splitRegionAndScatter(
+      // only perform region presplit if sample rdd is not empty
+      if(!sampleRDD.isEmpty()) {
+        tiContext.tiSession.splitRegionAndScatter(
         calculateSplitKeys(
           sampleRDD,
           dataSize,
@@ -264,6 +265,7 @@ class TiBatchWrite(@transient val df: DataFrame,
           regionSplitNumber
         ).map(k => k.bytes).asJava
       )
+      }
     }
 
     val deduplicatedTiRowRdd = deduplicateIfNecessary(tiRowRdd, tiTableInfo.isPkHandle)
