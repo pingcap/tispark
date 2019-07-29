e45a97dd8ca185dfe87926adee8d274e96ec9c00
Zhexuan Yang <yangzhexuan@pingcap.com>
2019-07-29 19:01:07 +0800

    adding test

---
+++ core/src/test/scala/org/apache/spark/sql/ViewTestSuite.scala
@@ -0,0 +1,16 @@
+package org.apache.spark.sql
+
+class ViewTestSuite extends BaseTiSparkTest {
+  private val table = "test_view"
+
+  test("Test View") {
+    tidbStmt.execute(s"drop table if exists $table")
+
+    tidbStmt.execute(s"create table $table(qty INT, price INT);")
+
+    tidbStmt.execute(s"INSERT INTO $table VALUES(3, 50);")
+    tidbStmt.execute(s"CREATE VIEW v AS SELECT qty, price, qty*price AS value FROM $table;")
+
+    judge(s"select * from $table")
+  }
+}
