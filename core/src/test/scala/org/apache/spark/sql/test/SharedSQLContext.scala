65ec6faff4aeeabbcefb4a2b5a44bc2feabf5487
marsishandsome <marsishandsome@gmail.com>
2019-08-16 09:52:03 +0800

    set tikv region size from 96M to 1M

--- core/src/test/scala/org/apache/spark/sql/test/SharedSQLContext.scala
+++ core/src/test/scala/org/apache/spark/sql/test/SharedSQLContext.scala
@@ -415,6 +415,7 @@ object SharedSQLContext extends Logging {
 
       sparkConf.set("spark.tispark.write.allow_spark_sql", "true")
       sparkConf.set("spark.tispark.write.without_lock_table", "true")
+      sparkConf.set("spark.tispark.tikv.region_split_size_in_mb", "1")
 
       if (isHiveEnabled) {
         // delete meta store directory to avoid multiple derby instances SPARK-10872
