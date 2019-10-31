14640db9857a4b4a693b844b2585dc4e6ac1ecc9
Zhexuan Yang <yangzhexuan@pingcap.com>
2019-10-31 21:59:22 +0800

    cherry-pick fix view (#1174) for release-2.2 (#1177)

--- core/src/test/scala/org/apache/spark/sql/ViewTestSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/ViewTestSuite.scala
@@ -4,17 +4,16 @@ class ViewTestSuite extends BaseTiSparkTest {
   private val table = "test_view"
 
   test("Test View") {
-    tidbStmt.execute(s"drop table if exists $table")
+    dropTbl()
+
+    tidbStmt.execute(s"create table $table(qty INT, price INT);")
+    tidbStmt.execute(s"INSERT INTO $table VALUES(3, 50);")
+
     try {
-      tidbStmt.execute("drop view if exists v")
+      tidbStmt.execute(s"CREATE VIEW v AS SELECT qty, price, qty*price AS value FROM $table;")
     } catch {
       case _: Exception => cancel
     }
-
-    tidbStmt.execute(s"create table $table(qty INT, price INT);")
-
-    tidbStmt.execute(s"INSERT INTO $table VALUES(3, 50);")
-    tidbStmt.execute(s"CREATE VIEW v AS SELECT qty, price, qty*price AS value FROM $table;")
 
     refreshConnections()
 
@@ -22,5 +21,17 @@ class ViewTestSuite extends BaseTiSparkTest {
     intercept[AnalysisException](spark.sql("select * from v"))
 
     spark.sql("show tables").show(false)
+  }
+
+  private def dropTbl() = {
+    tidbStmt.execute(s"drop table if exists $table")
+    try {
+      tidbStmt.execute("drop view if exists v")
+    } catch {
+      case _: Exception => cancel
+    }
+  }
+  override def afterAll() = {
+    dropTbl()
   }
 }
