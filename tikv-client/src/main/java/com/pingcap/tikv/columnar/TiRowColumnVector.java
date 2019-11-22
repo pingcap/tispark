dba3a22b26c3ee058ab9f15710da3d042973edef
zhexuany <yangzhexuan@pingcap.com>
2019-11-22 15:35:55 +0800

    cast to decimal

--- tikv-client/src/main/java/com/pingcap/tikv/columnar/TiRowColumnVector.java
+++ tikv-client/src/main/java/com/pingcap/tikv/columnar/TiRowColumnVector.java
@@ -127,7 +127,17 @@ public class TiRowColumnVector extends TiColumnVector {
    */
   @Override
   public BigDecimal getDecimal(int rowId, int precision, int scale) {
-    return (BigDecimal) rows[rowId].get(colIdx, null);
+    Object val = rows[rowId].get(colIdx, null);
+   if(val instanceof BigDecimal)  {
+     return (BigDecimal)val;
+   }
+
+   if(val instanceof Long) {
+     return BigDecimal.valueOf((long)val);
+   }
+
+   throw new UnsupportedOperationException(String.format("failed to getDecimal and the value is %s:%s",
+       val.getClass().getCanonicalName(), val));
   }
 
   /**
