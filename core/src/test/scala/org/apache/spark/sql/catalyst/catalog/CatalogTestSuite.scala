873866b936d7cbcdb67746fcb282f15eda5ba5f3
zhexuany <yangzhexuan@pingcap.com>
2020-01-08 11:31:02 +0800

    adding test cases

--- core/src/test/scala/org/apache/spark/sql/catalyst/catalog/CatalogTestSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/catalyst/catalog/CatalogTestSuite.scala
@@ -105,6 +105,9 @@ class CatalogTestSuite extends BaseTiSparkTest {
     spark.sql("desc t").show
     spark.sql("desc formatted t").show
     spark.sql("drop table if exists t")
+    spark.sql("create table state_part(District string,Enrolments string) PARTITIONED BY(state string)")
+    spark.sql("desc state_part")
+    spark.sql("drop table if exists state_part")
   }
 
   test("test support show columns") {
