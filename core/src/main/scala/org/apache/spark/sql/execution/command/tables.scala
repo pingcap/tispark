d995267d5f9d6537aef97bc41aa9cdac47f2c64f
zhexuany <yangzhexuan@pingcap.com>
2020-01-15 14:53:07 +0800

    adding tests

--- core/src/main/scala/org/apache/spark/sql/execution/command/tables.scala
+++ core/src/main/scala/org/apache/spark/sql/execution/command/tables.scala
@@ -65,7 +65,6 @@ case class TiShowTablesCommand(tiContext: TiContext, delegate: ShowTablesCommand
       val information = partition.simpleString
       Seq(Row(database, tableName, isTemp, s"$information\n"))
     }
-
   }
 }
 
