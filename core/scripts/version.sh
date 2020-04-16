cf6c0b3eb5e1e70bdd47692d1d46d75a9f25da15
Liangliang Gu <marsishandsome@gmail.com>
2020-04-16 12:56:52 +0800

    update version to 2.1.9-SNAPSHOT (#1396)

--- core/scripts/version.sh
+++ core/scripts/version.sh
@@ -16,7 +16,7 @@
 
 TISPARK_HOME="$(cd "`dirname "$0"`"/../..; pwd)"
 
-TiSparkReleaseVersion=2.1.8-SNAPSHOT
+TiSparkReleaseVersion=2.1.9-SNAPSHOT
 TiSparkBuildTS=`date -u '+%Y-%m-%d %I:%M:%S'`
 TiSparkGitHash=`git rev-parse HEAD`
 TiSparkGitBranch=`git rev-parse --abbrev-ref HEAD`
