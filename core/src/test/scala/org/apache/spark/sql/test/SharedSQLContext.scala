99a3761325b5369629695da87fdea49eda7acb03
Liangliang Gu <marsishandsome@gmail.com>
2020-05-07 17:00:03 +0800

    BatchWrite: fix split index (#1422)

--- core/src/test/scala/org/apache/spark/sql/test/SharedSQLContext.scala
+++ core/src/test/scala/org/apache/spark/sql/test/SharedSQLContext.scala
@@ -482,7 +482,8 @@ object SharedSQLContext extends Logging {
       TiDB_PASSWORD -> tidbPassword,
       TiDB_PORT -> s"$tidbPort",
       TiDB_USER -> tidbUser,
-      PD_ADDRESSES -> pdAddresses
+      PD_ADDRESSES -> pdAddresses,
+      "isTest" -> "true"
     )
   }
 }
