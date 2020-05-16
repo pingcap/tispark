472c0ee6b2e958483cd0399bf5938b61545b3495
birdstorm <samuelwyf@hotmail.com>
2020-05-16 16:41:53 +0800

    ignore case col2 is binary

--- core/src/test/scala/org/apache/spark/sql/types/MultiColumnDataTypeTest.scala
+++ core/src/test/scala/org/apache/spark/sql/types/MultiColumnDataTypeTest.scala
@@ -63,7 +63,8 @@ trait MultiColumnDataTypeTest extends BaseTiSparkTest {
       if (col1.contains("boolean") || col1.contains("blob0") || col1.contains("date") || col1
             .contains("bit")) {
         // ignore
-      } else if (col2.contains("boolean") || col2.contains("blob") || col2.contains("bit")) {
+      } else if (col2.contains("boolean") || col2.contains("blob") || col2.contains("bit") || col2
+                   .contains("binary")) {
         // ignore
       } else {
         test(query) {
