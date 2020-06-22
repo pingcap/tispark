00e32491bfe6206e3ee2ac9eb5e0ae31af241997
Soup <ilovesoup@gmail.com>
2020-06-22 09:11:42 +0800

    Delete TiSparkVersion.scala

--- core/src/main/scala/org/apache/spark/sql/tispark/TiSparkVersion.scala
+++
@@ -1,6 +0,0 @@
-package org.apache.spark.sql.tispark
-
-object TiSparkVersion {
-  val version: String =
-    "Release Version: 2.3.0-SNAPSHOT\nGit Commit Hash: a95a0ff6700845c22c89d659c15267768cbc3a2e\nGit Branch: HEAD\nUTC Build Time: 2019-11-21 01:13:29"
-}
