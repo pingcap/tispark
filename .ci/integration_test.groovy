592f1c9f396f5397fb0c38fc13ef174f62fa8212
marsishandsome <marsishandsome@gmail.com>
2020-06-24 15:12:50 +0800

    increase timeout

--- .ci/integration_test.groovy
+++ .ci/integration_test.groovy
@@ -319,7 +319,7 @@ def call(ghprbActualCommit, ghprbCommentBody, ghprbPullId, ghprbPullTitle, ghprb
                                     sh "ps aux | grep 'tiflash'"
                                 }
 
-                                timeout(120) {
+                                timeout(360) {
                                     run_test(chunk_suffix)
                                 }
                             } catch (err) {
