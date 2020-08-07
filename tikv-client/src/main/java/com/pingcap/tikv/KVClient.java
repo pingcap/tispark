333563408c39709dc6ab56e353ff32e8e5e65a90
birdstorm <samuelwyf@hotmail.com>
2020-08-07 13:34:58 +0800

    add invalidate region for batchGet

--- tikv-client/src/main/java/com/pingcap/tikv/KVClient.java
+++ tikv-client/src/main/java/com/pingcap/tikv/KVClient.java
@@ -201,6 +201,7 @@ public class KVClient implements AutoCloseable {
             } catch (final TiKVException e) {
               // TODO: any elegant way to re-split the ranges if fails?
               singleBatchBackOffer.doBackOff(BackOffFunction.BackOffFuncType.BoRegionMiss, e);
+              clientBuilder.getRegionManager().invalidateRegion(batch.region.getId());
               logger.warn("ReSplitting ranges for BatchGetRequest");
               // recursive calls
               return batchGet(singleBatchBackOffer, batch.keys, version);
