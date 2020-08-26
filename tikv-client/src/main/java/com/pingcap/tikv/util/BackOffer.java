802a03b5cbe3e0ec523229e0dc21e560b8a365af
Liangliang Gu <marsishandsome@gmail.com>
2020-08-26 16:10:13 +0800

    prewrite primary key backoffer use prewriteBackOfferMS (#1583)

--- tikv-client/src/main/java/com/pingcap/tikv/util/BackOffer.java
+++ tikv-client/src/main/java/com/pingcap/tikv/util/BackOffer.java
@@ -17,8 +17,6 @@
 
 package com.pingcap.tikv.util;
 
-import com.pingcap.tikv.TTLManager;
-
 public interface BackOffer {
   // Back off types.
   int seconds = 1000;
@@ -35,7 +33,6 @@ public interface BackOffer {
   int GC_DELETE_RANGE_MAX_BACKOFF = 100 * seconds;
   int RAWKV_MAX_BACKOFF = 40 * seconds;
   int SPLIT_REGION_BACKOFF = 20 * seconds;
-  int BATCH_PREWRITE_BACKOFF = TTLManager.MANAGED_LOCK_TTL;
   int BATCH_COMMIT_BACKOFF = 10 * seconds;
   int PD_INFO_BACKOFF = 5 * seconds;
 
