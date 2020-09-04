9a248e1c3290ffc808ad9dc0b7a48d1c7bda0cc3
Liangliang Gu <marsishandsome@gmail.com>
2020-09-04 14:18:25 +0800

    update writeThreadPerTask default value to 2 (#1600)

--- core/src/main/scala/com/pingcap/tispark/write/TiDBOptions.scala
+++ core/src/main/scala/com/pingcap/tispark/write/TiDBOptions.scala
@@ -90,7 +90,7 @@ class TiDBOptions(@transient val parameters: CaseInsensitiveMap[String]) extends
   val txnCommitBatchSize: Long = getOrDefault(TIDB_TXN_COMMIT_BATCH_SIZE, "16384").toLong
   // 32 * 1024
   val writeBufferSize: Int = getOrDefault(TIDB_WRITE_BUFFER_SIZE, "32768").toInt
-  val writeThreadPerTask: Int = getOrDefault(TIDB_WRITE_THREAD_PER_TASK, "1").toInt
+  val writeThreadPerTask: Int = getOrDefault(TIDB_WRITE_THREAD_PER_TASK, "2").toInt
   val retryCommitSecondaryKey: Boolean =
     getOrDefault(TIDB_RETRY_COMMIT_SECONDARY_KEY, "true").toBoolean
 
