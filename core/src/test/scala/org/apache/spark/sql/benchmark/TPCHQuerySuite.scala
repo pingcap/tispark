d8d9de4249333962591a6a6636554ae42f8201de
marsishandsome <marsishandsome@gmail.com>
2020-10-20 10:32:01 +0800

    add issue

--- core/src/test/scala/org/apache/spark/sql/benchmark/TPCHQuerySuite.scala
+++ core/src/test/scala/org/apache/spark/sql/benchmark/TPCHQuerySuite.scala
@@ -38,7 +38,8 @@ class TPCHQuerySuite extends BaseTiSparkTest {
     "q12",
     "q13",
     "q14",
-    //"q15",
+    // https://github.com/pingcap/tispark/issues/1624
+    // "q15",
     "q16",
     "q17",
     "q18",
