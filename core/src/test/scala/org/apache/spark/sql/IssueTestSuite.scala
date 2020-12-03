b338877e60ab9df9c9c7d29823b88e6164a23cf1
marsishandsome <marsishandsome@gmail.com>
2020-12-03 17:20:02 +0800

    add test: select count(a, b) with null values

--- core/src/test/scala/org/apache/spark/sql/IssueTestSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/IssueTestSuite.scala
@@ -21,6 +21,16 @@ import org.apache.spark.sql.functions.{col, sum}
 
 class IssueTestSuite extends BaseTiSparkTest {
 
+  test("count(a, b) with null values") {
+    tidbStmt.execute("drop table if exists t1")
+    tidbStmt.execute("create table t1(a int, b int)")
+    tidbStmt.execute("insert into t1 values (1, 1), (2, null), (null, 3)")
+
+    spark.sql("explain select count(a, b) from t1").show(false)
+    spark.sql("select count(a, b) from t1").show(false)
+    assert(1 == spark.sql("select count(a, b) from t1").collect().head.get(0))
+  }
+
   test("count(*) with null values") {
     tidbStmt.execute("drop table if exists t1")
     tidbStmt.execute("create table t1(a int)")
