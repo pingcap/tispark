b0b139181aa6590082fde3a2ba21b63a999e6c0e
marsishandsome <marsishandsome@gmail.com>
2020-12-03 10:48:21 +0800

    fix test

--- core/src/test/scala/com/pingcap/tispark/datasource/AutoRandomSuite.scala
+++ core/src/test/scala/com/pingcap/tispark/datasource/AutoRandomSuite.scala
@@ -21,6 +21,10 @@ import org.apache.spark.sql.types._
 
 class AutoRandomSuite extends BaseBatchWriteTest("test_datasource_auto_random") {
   test("auto random column insert null") {
+    if (!supportAutoRandom) {
+      cancel("current version of TiDB does not support auto random or is disabled!")
+    }
+
     val row = Row(null)
     val schema = StructType(List(StructField("i", LongType)))
 
@@ -35,6 +39,10 @@ class AutoRandomSuite extends BaseBatchWriteTest("test_datasource_auto_random") 
   }
 
   test("auto random column insert not null") {
+    if (!supportAutoRandom) {
+      cancel("current version of TiDB does not support auto random or is disabled!")
+    }
+
     val row = Row(1L)
     val schema = StructType(List(StructField("i", LongType)))
 
@@ -46,5 +54,18 @@ class AutoRandomSuite extends BaseBatchWriteTest("test_datasource_auto_random") 
     assert(
       caught.getMessage.equals(
         "tispark currently does not support write data to table with auto random column!"))
+  }
+
+  protected lazy val supportAutoRandom: Boolean = {
+    var result = true
+    tidbStmt.execute("drop table if exists t")
+    try {
+      jdbcUpdate(s"create table t(i bigint primary key NOT NULL AUTO_RANDOM)")
+    } catch {
+      case e: Throwable => result = false
+    } finally {
+      tidbStmt.execute("drop table if exists t")
+    }
+    result
   }
 }
