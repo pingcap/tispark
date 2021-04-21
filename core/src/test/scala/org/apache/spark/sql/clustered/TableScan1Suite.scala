86dc1c4e98f8b5daa05b3966a8a55900d080c78b
marsishandsome <marsishandsome@gmail.com>
2021-04-21 16:15:45 +0800

    refactor database random test

--- core/src/test/scala/org/apache/spark/sql/clustered/TableScan1Suite.scala
+++ core/src/test/scala/org/apache/spark/sql/clustered/TableScan1Suite.scala
@@ -15,17 +15,24 @@
 
 package org.apache.spark.sql.clustered
 
-import org.apache.spark.sql.test.generator.DataType.INT
+import com.pingcap.tispark.test.generator.DataType.INT
+import com.pingcap.tispark.test.generator.NullableType
 
 class TableScan1Suite extends ClusteredIndexTest {
-  test("table scan 1: primary key has two columns") {
+  test("table scan 0: primary key has two columns") {
     if (!supportClusteredIndex) {
       cancel("currently tidb instance does not support clustered index")
     }
-    for (dataType1 <- testDataTypes) {
-      for (dataType2 <- testDataTypes) {
-        val schemas = genSchema(List(dataType2, dataType1, INT, INT), tablePrefix)
-        schemas.foreach { schema =>
+    for (dataType1 <- testDataTypes2) {
+      for (dataType2 <- testDataTypes1) {
+        val schemaAndDataList = genSchemaAndData(
+          rowCount,
+          List(dataType2, dataType1, INT, INT).map(d =>
+            genDescription(d, NullableType.NumericNotNullable)),
+          database,
+          isClusteredIndex = true,
+          hasTiFlashReplica = enableTiFlashTest)
+        schemaAndDataList.foreach { schema =>
           test(schema)
         }
       }
