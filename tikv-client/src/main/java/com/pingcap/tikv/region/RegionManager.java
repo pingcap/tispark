97de9301ad014db9857a08bc4893d706d5695a0d
birdstorm <samuelwyf@hotmail.com>
2021-08-13 11:43:06 +0800

    fix equals
    Signed-off-by: birdstorm <samuelwyf@hotmail.com>

--- tikv-client/src/main/java/com/pingcap/tikv/region/RegionManager.java
+++ tikv-client/src/main/java/com/pingcap/tikv/region/RegionManager.java
@@ -275,7 +275,7 @@ public class RegionManager {
           logger.debug(String.format("invalidateRegion ID[%s]", region.getId()));
         }
         TiRegion oldRegion = regionCache.get(region.getId());
-        if (oldRegion != null && oldRegion == region) {
+        if (oldRegion != null && oldRegion.equals(region)) {
           keyToRegionIdCache.remove(makeRange(region.getStartKey(), region.getEndKey()));
           regionCache.remove(region.getId());
         }
