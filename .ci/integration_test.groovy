d7e219e7680d2a6449d500851986b60137be44f1
shiyuhang <1136742008@qq.com>
2021-12-21 13:58:34 +0800

    testci:exclude PartitionTableSuite

--- .ci/integration_test.groovy
+++ .ci/integration_test.groovy
@@ -9,7 +9,7 @@ def call(ghprbActualCommit, ghprbCommentBody, ghprbPullId, ghprbPullTitle, ghprb
     def TIFLASH_BRANCH = "master"
     def MVN_PROFILE = "-Pjenkins"
     def TEST_MODE = "full"
-    def PARALLEL_NUMBER = 54
+    def PARALLEL_NUMBER = 18
     def TEST_REGION_SIZE = "normal"
     def TEST_TIFLASH = "false"
     def TEST_ALTER_PRIMARY_KEY = "true"
