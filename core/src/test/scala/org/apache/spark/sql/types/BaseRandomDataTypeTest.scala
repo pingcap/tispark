6c0749084c553f15ec7887fffbd1992936a35f2e
shiyuhang <1136742008@qq.com>
2022-04-21 16:43:15 +0800

    fmt

--- core/src/test/scala/org/apache/spark/sql/types/BaseRandomDataTypeTest.scala
+++ core/src/test/scala/org/apache/spark/sql/types/BaseRandomDataTypeTest.scala
@@ -51,8 +51,8 @@ trait BaseRandomDataTypeTest extends BaseTiSparkTest with RandomTest {
     initSQLList.foreach { sql =>
       try {
         tidbStmt.execute(sql)
-        // we need to sleep 2s to wait schme changed, or wirte will throw `schema has changed during prewrite!`
-        if(sql.contains("SET TIFLASH REPLICA")){
+        // we need to sleep 2s to wait schema changed, or wirte will throw `schema has changed during prewrite!`
+        if (sql.contains("SET TIFLASH REPLICA")) {
           Thread.sleep(2000)
         }
         println(sql)
