98fddb8ce5b63c31c3ab475c687eb89d19ac407b
Daemonxiao <735462752@qq.com>
2022-05-23 21:35:53 +0800

    fix shade netty.tcnative

--- core/src/test/scala/org/apache/spark/sql/test/SharedSQLContext.scala
+++ core/src/test/scala/org/apache/spark/sql/test/SharedSQLContext.scala
@@ -348,7 +348,8 @@ trait SharedSQLContext
 
     var SSLPara = "false"
     if (conf.contains("jdbc.tls_enable") && conf.get("jdbc.tls_enable").equals("true")) {
-      SSLPara = "true&verifyServerCertificate=false&requireSSL=true&enabledTLSProtocols=TLSv1.2"
+      SSLPara =
+        "true&verifyServerCertificate=false&requireSSL=true&enabledTLSProtocols=TLSv1.2,TLSv1.3"
     }
 
     jdbcUrl =
