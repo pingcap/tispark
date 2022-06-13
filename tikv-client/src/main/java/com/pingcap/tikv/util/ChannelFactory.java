fa3030f9c7e8034dedece6aba43cfb401f0c916a
Daemonxiao <735462752@qq.com>
2022-06-13 18:51:24 +0800

    fix TLS reload doesn't work after delete cert file

--- tikv-client/src/main/java/com/pingcap/tikv/util/ChannelFactory.java
+++ tikv-client/src/main/java/com/pingcap/tikv/util/ChannelFactory.java
@@ -77,9 +77,15 @@ public class ChannelFactory implements AutoCloseable {
           this::tryReload, pollInterval, pollInterval, TimeUnit.SECONDS);
     }
 
+    // If any execution of the task encounters an exception, subsequent executions are suppressed.
     private void tryReload() {
-      if (needReload()) {
-        onChange.run();
+      // Add exception handling to avoid schedule stop.
+      try {
+        if (needReload()) {
+          onChange.run();
+        }
+      } catch (Exception e) {
+        logger.error("Failed to reload cert!" + e);
       }
     }
 
@@ -166,11 +172,16 @@ public class ChannelFactory implements AutoCloseable {
     @Override
     public SslContextBuilder createSslContextBuilder() {
       SslContextBuilder builder = GrpcSslContexts.forClient().protocols("TLSv1.2", "TLSv1.3");
-      if (trustPath != null) {
-        builder.trustManager(new File(trustPath));
-      }
-      if (chainPath != null && keyPath != null) {
-        builder.keyManager(new File(chainPath), new File(keyPath));
+      try {
+        if (trustPath != null) {
+          builder.trustManager(new File(trustPath));
+        }
+        if (chainPath != null && keyPath != null) {
+          builder.keyManager(new File(chainPath), new File(keyPath));
+        }
+      } catch (Exception e) {
+        logger.error("PEM SSL context builder failed!", e);
+        throw new IllegalArgumentException(e);
       }
       return builder;
     }
