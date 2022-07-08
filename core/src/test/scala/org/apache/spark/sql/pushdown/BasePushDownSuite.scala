7ddd3346637d7338cc302865e409b53e9c66d98d
shiyuhang <1136742008@qq.com>
2022-07-08 12:17:01 +0800

    fmt

--- core/src/test/scala/org/apache/spark/sql/pushdown/BasePushDownSuite.scala
+++ core/src/test/scala/org/apache/spark/sql/pushdown/BasePushDownSuite.scala
@@ -20,7 +20,7 @@ package org.apache.spark.sql.pushdown
 
 import org.apache.spark.sql.catalyst.plans.BasePlanTest
 
-trait BasePushDownSuite extends BasePlanTest{
+trait BasePushDownSuite extends BasePlanTest {
 
   override def beforeAll(): Unit = {
     super.beforeAll()
@@ -28,7 +28,7 @@ trait BasePushDownSuite extends BasePlanTest{
     insertTable
   }
 
-  def createTable: Unit ={
+  def createTable: Unit = {
     tidbStmt.execute("DROP TABLE IF EXISTS `full_data_type_table_pk`")
     tidbStmt.execute("""
          CREATE TABLE `full_data_type_table_pk` (
@@ -97,7 +97,7 @@ trait BasePushDownSuite extends BasePlanTest{
       """)
   }
 
-  def insertTable: Unit ={
+  def insertTable: Unit = {
     tidbStmt.execute("insert into full_data_type_table_pk (id_dt) values(1)")
     tidbStmt.execute(
       "INSERT INTO `full_data_type_table_pk` VALUES (-1000,'a948ddcf-9053-4700-916c-983d4af895ef','2017-11-02 00:00:00','23333','66','2017-11-02','2017-11-02 08:43:23',2017,4355836469450447576,91598606438408806444.969071534481121418,0.2054466,0.28888312,1238733782,1866044,0.09951998980446786,17438,75,'Y0FAww8awb','Y1wnS','jXatvse4l43dWcunQgGhBk4m5jyGdLWjq3DxwM6L3CWhvJAPIEP5TzIVHzE26IDHlQAu3356kyIpfArJWJHGUOScatBqERPVQn6daA7YL5zH0rr8JnRkzKhIKX5jtkSVwdeROiSM9x5faPqxdvWHq0pkKLsJjUrgoIibl6UM5eR0h8VbdbwiRYEiyp5kvnrGLs6L9xKuDYBVG1tCt9CosNLjFNZtWs0miozZomPsSI1kt3iSn4S8eVMZGlMRA9BJ1zgGBVugHPmJ6b3B7EI4wOKTbUHmg67HV4P8RXL3dsdkX0SN6O5fQUdeZS3Rx7LNRo3w2ET4EOHcRh4qEvVnWxIVMZzljy4BTwooJMYGlyZ5iVhtzhL9I9J1IeZsW5a2emi2gmzn4PqMiE3B4XG27KlMliAPbhvl3wWrxGYwu2SenibyS4fKg0qRjdTS8MOnBaGiAMYfWOZWyJNR1JkxD3q8vRCgq92FILErE9bved60Fvuv5xtH','fXPKRoPf2ETSF4I7a4eroztWS26ja6HlB8wnwgBByCaUPKCCnoyxp8Y3408mRUTn2NMPY4tf5wnqIbFmDkWEhC0EStMAjGMack24','MlDOt5EX4bnaVHn5lX8kZtSrBDNSdaRMEeh8uUcOzfVPJT493F','MSBxzzDYGOggZxDUadO9','\\0','16:43:23','1','a,b')")
