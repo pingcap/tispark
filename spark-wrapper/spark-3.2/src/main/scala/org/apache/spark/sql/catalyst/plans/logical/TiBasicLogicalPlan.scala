cef40db5c9cae1758bc7433401a38ea3e51bcada
qidi1 <1083369179@qq.com>
2022-08-05 09:18:00 +0800

    update license
    Signed-off-by: qidi1 <1083369179@qq.com>

--- spark-wrapper/spark-3.2/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/TiBasicLogicalPlan.scala
+++ spark-wrapper/spark-3.2/src/main/scala/org/apache/spark/sql/catalyst/plans/logical/TiBasicLogicalPlan.scala
@@ -17,15 +17,18 @@
 package org.apache.spark.sql.catalyst.plans.logical
 
 import com.pingcap.tispark.auth.TiAuthorization
-import org.apache.spark.sql.catalyst.analysis.ResolvedNamespace
+import org.apache.spark.sql.catalyst.plans.logical.{LogicalPlan, SetCatalogAndNamespace}
 
 class TiBasicLogicalPlan {
   def verifyAuthorizationRule(
       logicalPlan: LogicalPlan,
       tiAuthorization: Option[TiAuthorization]): LogicalPlan = {
     logicalPlan match {
-      case st @ SetCatalogAndNamespace(ResolvedNamespace(catalog, ns)) =>
-        ns.foreach(TiAuthorization.authorizeForSetDatabase(_, tiAuthorization))
+      case st @ SetCatalogAndNamespace(catalogManager, catalogName, namespace) =>
+        if (namespace.isDefined) {
+          namespace.get
+            .foreach(TiAuthorization.authorizeForSetDatabase(_, tiAuthorization))
+        }
         st
     }
   }
