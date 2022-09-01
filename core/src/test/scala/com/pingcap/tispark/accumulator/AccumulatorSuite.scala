7be151a8c2bac3c0c109d67ca882348b3f4226a7
qidi1 <1083369179@qq.com>
2022-09-01 22:10:31 +0800

    format code
    Signed-off-by: qidi1 <1083369179@qq.com>

--- core/src/test/scala/com/pingcap/tispark/accumulator/AccumulatorSuite.scala
+++ core/src/test/scala/com/pingcap/tispark/accumulator/AccumulatorSuite.scala
@@ -1,22 +1,38 @@
+/*
+ *
+ * Copyright 2022 PingCAP, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.pingcap.tispark.accumulator;
 
 import org.apache.log4j.spi.LoggingEvent
 import org.apache.log4j.{AppenderSkeleton, Logger}
 import org.apache.spark.sql.BaseTiSparkTest
-import org.apache.spark.sql.test.SharedSQLContext
 
 import java.util
 import java.util.stream.Collectors;
 
-class AccumulatorSuite extends BaseTiSparkTest{
+class AccumulatorSuite extends BaseTiSparkTest {
   test("cacheInvalidateCallback does not work") {
     val listLogAppender = new ListLogAppender
     val logger = Logger.getRootLogger
     logger.addAppender(listLogAppender)
     try {
       tidbStmt.execute("DROP TABLE IF EXISTS `t1`")
-      tidbStmt.execute(
-        """
+      tidbStmt.execute("""
           |CREATE TABLE `t1` (
           |`a` BIGINT(20)  NOT NULL,
           |`b` varchar(255) NOT NULL,
@@ -30,9 +46,10 @@ class AccumulatorSuite extends BaseTiSparkTest{
     } finally {
       logger.removeAppender(listLogAppender)
     }
-    val pdCacheInvalidateListenerLog = listLogAppender.getLog.stream().filter(
-      e => e.getLoggerName == "com.pingcap.tispark.listener.PDCacheInvalidateListener").
-      collect(Collectors.toList[LoggingEvent])
+    val pdCacheInvalidateListenerLog = listLogAppender.getLog
+      .stream()
+      .filter(e => e.getLoggerName == "com.pingcap.tispark.listener.PDCacheInvalidateListener")
+      .collect(Collectors.toList[LoggingEvent])
     assert(pdCacheInvalidateListenerLog.size() == 1)
   }
 
@@ -46,8 +63,7 @@ class AccumulatorSuite extends BaseTiSparkTest{
       log.add(loggingEvent)
     }
 
-    override def close(): Unit = {
-    }
+    override def close(): Unit = {}
 
     def getLog = new util.ArrayList[LoggingEvent](log)
   }
