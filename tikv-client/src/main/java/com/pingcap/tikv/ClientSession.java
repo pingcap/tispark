1b08125aafd3de5085c3f2f612d098c88b5c894b
shiyuhang <1136742008@qq.com>
2022-09-19 10:45:16 +0800

    test getinstance

--- tikv-client/src/main/java/com/pingcap/tikv/ClientSession.java
+++ tikv-client/src/main/java/com/pingcap/tikv/ClientSession.java
@@ -70,7 +70,8 @@ public class ClientSession implements AutoCloseable {
       this.conf = com.pingcap.tikv.TiConfiguration.createDefault("127.0.0.1:2379");
     }
     this.tiKVSession =
-        org.tikv.common.TiSession.create(ConvertUpstreamUtils.convertTiConfiguration(getConf()));
+        org.tikv.common.TiSession.getInstance(
+            ConvertUpstreamUtils.convertTiConfiguration(getConf()));
     refreshNewCollationEnabled();
   }
 
