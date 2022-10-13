d20146666b2cb4012c926ec482a97db534c8712e
yangxin <yangxin@pingcap.com>
2022-10-13 15:30:29 +0800

    Add test for partition pruning when using new collation

--- core/src/test/scala/com/pingcap/tispark/partition/PartitionNewCollationSuite.scala
+++ core/src/test/scala/com/pingcap/tispark/partition/PartitionNewCollationSuite.scala
@@ -20,6 +20,7 @@ import com.pingcap.tikv.meta.Collation
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
+import org.scalatest.Matchers
 import org.scalatest.Matchers.{contain, convertToAnyShouldWrapper}
 
 class PartitionNewCollationSuite extends PartitionBaseSuite {
@@ -146,6 +147,44 @@ class PartitionNewCollationSuite extends PartitionBaseSuite {
     val insertResultSpark = spark.sql(s"select * from `tidb_catalog`.`$database`.`$table`")
     insertResultSpark.collect() should contain theSameElementsAs Array(Row(57L, "ß"))
     checkPartitionJDBCResult(Map("p0" -> Array(), "p1" -> Array(Array(57L, "ß"))))
+
+    tidbStmt.execute(s"ADMIN CHECK TABLE `$database`.`$table`")
+  }
+
+  test("range column read with utf8mb4_unicode_ci collation") {
+    checkNewCollationEnabled()
+    tidbStmt.execute(
+      s"create table `$database`.`$table` (id bigint, name varchar(16) unique key) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci partition by range columns(name) (" +
+        s"partition p0 values less than ('d')," +
+        s"partition p1 values less than ('t')," +
+        s"partition p2 values less than MAXVALUE)")
+
+    tidbStmt.execute(s"insert into `$database`.`$table` VALUES (17, 'f')")
+
+    // tispark will prune partition ang query p0, p1
+    val insertResultSpark =
+      spark.sql(s"select * from `tidb_catalog`.`$database`.`$table` where name < 'G'")
+    insertResultSpark.collect() should contain theSameElementsAs Array(Row(17L, "f"))
+    checkPartitionJDBCResult(Map("p0" -> Array(), "p1" -> Array(Array(17L, "f"))))
+
+    tidbStmt.execute(s"ADMIN CHECK TABLE `$database`.`$table`")
+  }
+
+  test("range column read with utf8mb4_bin collation") {
+    checkNewCollationEnabled()
+    tidbStmt.execute(
+      s"create table `$database`.`$table` (id bigint, name varchar(16) unique key) CHARSET utf8mb4 COLLATE utf8mb4_bin partition by range columns(name) (" +
+        s"partition p0 values less than ('d')," +
+        s"partition p1 values less than ('t')," +
+        s"partition p2 values less than MAXVALUE)")
+
+    tidbStmt.execute(s"insert into `$database`.`$table` VALUES (17, 'f')")
+
+    // tispark will prune partition ang only query p0
+    val insertResultSpark =
+      spark.sql(s"select * from `tidb_catalog`.`$database`.`$table` where name < 'G'")
+    insertResultSpark.collect() shouldBe Matchers.empty
+    checkPartitionJDBCResult(Map("p0" -> Array(), "p1" -> Array(Array(17L, "f"))))
 
     tidbStmt.execute(s"ADMIN CHECK TABLE `$database`.`$table`")
   }
