44826d97eb019c2eb7b65c5df412f305ea47dee4
shiyuhang <1136742008@qq.com>
2022-10-31 16:21:42 +0800

    add test

--- tikv-client/src/main/java/com/pingcap/tikv/region/RegionManager.java
+++ tikv-client/src/main/java/com/pingcap/tikv/region/RegionManager.java
@@ -141,8 +141,6 @@ public class RegionManager {
     return Pair.create(region, store);
   }
 
-  public Store balanceTiFlashStore() {}
-
   public Store getStoreById(long id) {
     return getStoreById(id, ConcreteBackOffer.newGetBackOff());
   }
