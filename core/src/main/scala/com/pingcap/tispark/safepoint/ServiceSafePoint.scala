7bfa6579812788bee09ad8e3a42ea5d3f035fd7a
shiyuhang <1136742008@qq.com>
2023-02-28 14:17:53 +0800

    optimize log
    Signed-off-by: shiyuhang <1136742008@qq.com>

--- core/src/main/scala/com/pingcap/tispark/safepoint/ServiceSafePoint.scala
+++ core/src/main/scala/com/pingcap/tispark/safepoint/ServiceSafePoint.scala
@@ -28,7 +28,7 @@ case class ServiceSafePoint(
         if (safePoint > minStartTs) {
           // will not happen unless someone delete the TiSpark service safe point in PD compulsively
           logger.error(
-            s"Failed to register service GC safe point because the current minimum safe point $safePoint is newer than what we assume $minStartTs. Maybe you delete the TiSpark safe point in PD")
+            s"Failed to register service GC safe point because the current minimum safe point $safePoint is newer than what we assume $minStartTs. Maybe you delete the TiSpark safe point in PD.")
         } else {
           logger.info(s"register service GC safe point $minStartTs success.")
         }
@@ -44,7 +44,7 @@ case class ServiceSafePoint(
       val now = tiSession.getTimestamp
       if (now.getPhysical - startTimeStamp.getPhysical >= GCMaxWaitTime * 1000) {
         throw new TiInternalException(
-          s"Can not pause GC more than spark.tispark.gc_max_wait_time=$GCMaxWaitTime s. start_ts: ${startTimeStamp.getVersion}, now: ${now.getVersion}. You can adjust spark.tispark.gc_max_wait_time to increase the gc max wait time")
+          s"Can not pause GC more than spark.tispark.gc_max_wait_time=$GCMaxWaitTime s. start_ts: ${startTimeStamp.getVersion}, now: ${now.getVersion}. You can adjust spark.tispark.gc_max_wait_time to increase the gc max wait time.")
       }
       val startTs = startTimeStamp.getVersion
       if (startTs >= minStartTs) {
@@ -67,8 +67,9 @@ case class ServiceSafePoint(
       ConcreteBackOffer.newCustomBackOff(BackOffer.PD_UPDATE_SAFE_POINT_BACKOFF))
     if (safePoint > startTs) {
       throw new TiInternalException(
-        s"Failed to register service GC safe point because the current minimum safe point $safePoint is newer than what we assume $startTs")
+        s"Failed to check service GC safe point because the current minimum safe point $safePoint is newer than start_ts $startTs.")
     }
+    logger.info(s"check start_ts $startTs success.")
   }
 
   private def applyServiceSafePoint(startTs: Long): Unit = {
@@ -79,8 +80,9 @@ case class ServiceSafePoint(
       ConcreteBackOffer.newCustomBackOff(BackOffer.PD_UPDATE_SAFE_POINT_BACKOFF))
     if (safePoint > startTs) {
       throw new TiInternalException(
-        s"Failed to register service GC safe point because the current minimum safe point $safePoint is newer than what we assume $startTs")
+        s"Failed to register service GC safe point because the current minimum safe point $safePoint is newer than what we assume $startTs.")
     }
+    logger.info(s"register service GC safe point $startTs success.")
   }
 
   def stopRegisterSafePoint(): Unit = {
