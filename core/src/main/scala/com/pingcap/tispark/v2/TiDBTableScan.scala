f9c029cdf7780f04040efdf5cd5049f0521bd369
Guangggggg <guangqin.hao@haoguangqindeMacBook-Pro.local>
2023-05-26 15:54:08 +0800

    Resolve the problem of the generating of BroadcastJoin in physical plan in some cases

---
+++ core/src/main/scala/com/pingcap/tispark/v2/TiDBTableScan.scala
@@ -0,0 +1,26 @@
+package com.pingcap.tispark.v2
+
+import com.pingcap.tispark.TiTableReference
+import org.apache.spark.sql.connector.read.{Scan, Statistics, SupportsReportStatistics}
+import org.apache.spark.sql.types.StructType
+
+import java.util.OptionalLong
+
+case class TiDBTableScan(
+     tableRef: TiTableReference,
+     schema: StructType)
+  extends Scan
+    with SupportsReportStatistics {
+
+  override def readSchema(): StructType = schema
+
+  override def estimateStatistics(): Statistics = {
+    new Statistics {
+      override def sizeInBytes(): OptionalLong = {
+        val size = tableRef.sizeInBytes
+        OptionalLong.of(size)
+      }
+      override def numRows(): OptionalLong = OptionalLong.empty()
+    }
+  }
+}
