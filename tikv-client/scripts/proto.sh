be6fc7b8c258ff93624b46728f7fbe29b4c7a56c
shiyuhang <1136742008@qq.com>
2025-01-03 13:36:39 +0800

    fix tipb

--- tikv-client/scripts/proto.sh
+++ tikv-client/scripts/proto.sh
@@ -18,7 +18,7 @@ CURRENT_DIR=`pwd`
 TISPARK_HOME="$(cd "`dirname "$0"`"/../..; pwd)"
 cd $TISPARK_HOME/tikv-client
 
-tipb_hash=4d69c6f95e683dfb5859277563bf896aca06ec34
+tipb_hash=07c1d4cf43236a98d6299afbe864b628b52c0eae
 
 if [ -d "tipb" ]; then
 	cd tipb; git fetch -p; git checkout ${tipb_hash}; cd ..
